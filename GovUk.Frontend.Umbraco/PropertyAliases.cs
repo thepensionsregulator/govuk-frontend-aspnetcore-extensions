b2f2621222a0a602b15992a93caf13fc6179dd54
Rick Mason <rick.mason@tpr.gov.uk>
2022-05-23 17:50:58 +0100

    Add unit tests for displaying custom server-side error messages from Umbraco AB#126930

---
+++ GovUk.Frontend.Umbraco/PropertyAliases.cs
@@ -0,0 +1,16 @@
+﻿namespace GovUk.Frontend.Umbraco
+{
+    internal static class PropertyAliases
+    {
+        internal const string ModelProperty = "modelProperty";
+        internal const string ErrorMessageRequired = "errorMessageRequired";
+        internal const string ErrorMessageRegex = "errorMessageRegex";
+        internal const string ErrorMessageEmail = "errorMessageEmail";
+        internal const string ErrorMessageLength = "errorMessageLength";
+        internal const string ErrorMessageMinLength = "errorMessageMinLength";
+        internal const string ErrorMessageMaxLength = "errorMessageMaxLength";
+        internal const string ErrorMessageRange = "errorMessageRange";
+        internal const string ErrorMessageCompare = "errorMessageCompare";
+        internal const string ErrorMessageCreditCard = "errorMessageCreditCard";
+    }
+}

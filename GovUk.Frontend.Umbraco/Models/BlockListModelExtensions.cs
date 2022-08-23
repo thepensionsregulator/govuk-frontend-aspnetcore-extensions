27f821db93960a40594309f59c7b04f44e8978eb
paul taylor <paul.taylor@tpr.gov.uk>
2022-08-23 10:17:47 +0100

    #108343 derive blocklist from published properties

--- GovUk.Frontend.Umbraco/Models/BlockListModelExtensions.cs
+++ GovUk.Frontend.Umbraco/Models/BlockListModelExtensions.cs
@@ -1,7 +1,9 @@
 ﻿using System;
 using System.Collections.ObjectModel;
+using System.Linq;
 using Umbraco.Cms.Core;
 using Umbraco.Cms.Core.Models.Blocks;
+using Umbraco.Cms.Core.Routing;
 using Umbraco.Extensions;
 
 namespace GovUk.Frontend.Umbraco.Models
@@ -17,6 +19,38 @@ namespace GovUk.Frontend.Umbraco.Models
         public static BlockListItem? FindBlock(this ReadOnlyCollection<BlockListItem> blockList, Func<BlockListItem, bool> matcher)
         {
             return RecursivelyFindBlock(blockList, matcher);
+        }
+
+        /// <summary>
+        /// Recursively find the first block in a request that is bound to a model property using the 'Model property' Umbraco property in the block's settings
+        /// </summary>
+        /// <param name="request"></param>
+        /// <param name="propertyName"></param>
+        /// <returns></returns>
+        /// <exception cref="ArgumentNullException"></exception>
+        public static BlockListItem? FindBlock(this IPublishedRequest request, string propertyName)
+        {
+            if (request?.PublishedContent?.Properties is null)
+            {
+                throw new ArgumentNullException(nameof(request.PublishedContent.Properties));
+            }
+
+            var blockLists = request.PublishedContent.Properties
+                .Where(x => x.PropertyType.EditorAlias == Constants.PropertyEditors.Aliases.BlockList && x.HasValue())
+                .Select(x => x.Value<BlockListModel>(null));
+
+            foreach (var blockList in blockLists)
+            {
+                if (blockList != null)
+                {
+                    var block = blockList.FindBlockByBoundProperty(propertyName);
+                    if (block != null)
+                    {
+                        return block;
+                    }
+                }
+            }
+            return null;
         }
 
         /// <summary>

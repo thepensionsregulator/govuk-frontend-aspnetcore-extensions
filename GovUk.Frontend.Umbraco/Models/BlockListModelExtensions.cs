ed4dbcd4609ba18429ef294b4de2c25d36587064
Rick Mason <rick.mason@tpr.gov.uk>
2022-08-25 17:50:17 +0100

    Update FindBlock to work for overridden blocks, and to have a shortcut method for finding by alias AB#108354

--- GovUk.Frontend.Umbraco/Models/BlockListModelExtensions.cs
+++ GovUk.Frontend.Umbraco/Models/BlockListModelExtensions.cs
@@ -1,5 +1,5 @@
 ﻿using System;
-using System.Collections.ObjectModel;
+using System.Collections.Generic;
 using Umbraco.Cms.Core;
 using Umbraco.Cms.Core.Models.Blocks;
 using Umbraco.Extensions;
@@ -14,7 +14,7 @@ namespace GovUk.Frontend.Umbraco.Models
         /// <param name="blockList">The block list to search</param>
         /// <param name="matcher">A function which returns <c>true</c> for a matching block and <c>false</c> otherwise</param>
         /// <returns>The first matching block, or <c>null</c> if no blocks are matched</returns>
-        public static BlockListItem? FindBlock(this ReadOnlyCollection<BlockListItem> blockList, Func<BlockListItem, bool> matcher)
+        public static BlockListItem? FindBlock(this IEnumerable<BlockListItem> blockList, Func<BlockListItem, bool> matcher)
         {
             return RecursivelyFindBlock(blockList, matcher);
         }
@@ -25,12 +25,23 @@ namespace GovUk.Frontend.Umbraco.Models
         /// <param name="blockList">The block list to search</param>
         /// <param name="propertyName">The name of the property on the view model (use <c>nameof(model.MyProperty)</c>)</param>
         /// <returns>The first matching block, or <c>null</c> if no blocks are matched</returns>
-        public static BlockListItem? FindBlockByBoundProperty(this ReadOnlyCollection<BlockListItem> blockList, string propertyName)
+        public static BlockListItem? FindBlockByBoundProperty(this IEnumerable<BlockListItem> blockList, string propertyName)
         {
             return RecursivelyFindBlock(blockList, x => x.Settings.GetProperty(PropertyAliases.ModelProperty)?.GetValue()?.ToString() == propertyName);
         }
 
-        private static BlockListItem? RecursivelyFindBlock(ReadOnlyCollection<BlockListItem> blockList, Func<BlockListItem, bool> matcher)
+        /// <summary>
+        /// Recursively find the first block in a block list where the content is of a given element type
+        /// </summary>
+        /// <param name="blockList">The block list to search</param>
+        /// <param name="alias">The alias of element type for the content of the block</param>
+        /// <returns>The first matching block, or <c>null</c> if no blocks are matched</returns>
+        public static BlockListItem? FindBlockByContentTypeAlias(this IEnumerable<BlockListItem> blockList, string alias)
+        {
+            return RecursivelyFindBlock(blockList, x => x.Content.ContentType.Alias == alias);
+        }
+
+        private static BlockListItem? RecursivelyFindBlock(IEnumerable<BlockListItem> blockList, Func<BlockListItem, bool> matcher)
         {
             if (blockList is null)
             {
@@ -48,7 +59,9 @@ namespace GovUk.Frontend.Umbraco.Models
                 {
                     if (blockProperty.PropertyType.EditorAlias == Constants.PropertyEditors.Aliases.BlockList && blockProperty.HasValue())
                     {
-                        var result = RecursivelyFindBlock(blockProperty.Value<BlockListModel>(null)!, matcher);
+                        IEnumerable<BlockListItem>? childBlocks = (block as OverridableBlockListItem)?.Content.Value<OverridableBlockListModel>(blockProperty.Alias);
+                        if (childBlocks == null) { childBlocks = blockProperty.Value<BlockListModel>(null); }
+                        var result = RecursivelyFindBlock(childBlocks!, matcher);
                         if (result != null)
                         {
                             return result;

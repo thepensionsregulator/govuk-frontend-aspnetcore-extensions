4b699fb9b867844421b021b8efd9612a5c0932f2
Samuel Hägglund <107886283+samuelhaa@users.noreply.github.com>
2022-11-09 16:44:11 +0000

    add class TaskListRow

---
+++ GovUk.Frontend.AspNetCore.Extensions/HtmlGeneration/TaskListRow.cs
@@ -0,0 +1,39 @@
+﻿using Microsoft.AspNetCore.Html;
+using Microsoft.AspNetCore.Mvc.ViewFeatures;
+using System.Collections.Generic;
+
+namespace GovUk.Frontend.AspNetCore.Extensions.HtmlGeneration
+{
+    public class TaskListRow
+    {
+        public TaskListRowKey? Key { get; set; }
+        public TaskListRowValue? Value { get; set; }
+        public AttributeDictionary? Attributes { get; set; }
+        public TaskListRowActions? Actions { get; set; }
+    }
+
+    public class TaskListRowActions
+    {
+        public IReadOnlyList<TaskListRowAction>? Items { get; set; }
+        public AttributeDictionary? Attributes { get; set; }
+    }
+
+    public class TaskListRowAction
+    {
+        public string? VisuallyHiddenText { get; set; }
+        public IHtmlContent? Content { get; set; }
+        public AttributeDictionary? Attributes { get; set; }
+    }
+
+    public class TaskListRowKey
+    {
+        public IHtmlContent? Content { get; set; }
+        public AttributeDictionary? Attributes { get; set; }
+    }
+
+    public class TaskListRowValue
+    {
+        public IHtmlContent? Content { get; set; }
+        public AttributeDictionary? Attributes { get; set; }
+    }
+}

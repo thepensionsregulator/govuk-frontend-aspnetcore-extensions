46da7f375e30d30eca31d9e5709e63b5d8417510
Samuel Hägglund <107886283+samuelhaa@users.noreply.github.com>
2022-11-09 16:35:28 +0000

    Add class TaskListRowActionTagHelper

---
+++ GovUk.Frontend.AspNetCore.Extensions/TagHelpers/TaskListRowActionTagHelper.cs
@@ -0,0 +1,42 @@
+﻿using GovUk.Frontend.AspNetCore.HtmlGeneration;
+using GovUk.Frontend.AspNetCore.TagHelpers;
+using Microsoft.AspNetCore.Razor.TagHelpers;
+using System.Threading.Tasks;
+
+namespace GovUk.Frontend.AspNetCore.Extensions.TagHelpers
+{
+    /// <summary>
+    /// Represents an action in a GDS task list row.
+    /// </summary>
+    [HtmlTargetElement(TagName, ParentTag = TaskListRowActionsTagHelper.TagName)]
+    [OutputElementHint(ComponentGenerator.TaskListRowActionElement)]
+    public class TaskListRowActionTagHelper : TagHelper
+    {
+        internal const string TagName = "govuk-task-list-row-action";
+
+        private const string VisuallyHiddenTextAttributeName = "visually-hidden-text";
+
+        /// <summary>
+        /// The visually hidden text for the action link.
+        /// </summary>
+        [HtmlAttributeName(VisuallyHiddenTextAttributeName)]
+        public string? VisuallyHiddenText { get; set; }
+
+        /// <inheritdoc/>
+        public override async Task ProcessAsync(TagHelperContext context, TagHelperOutput output)
+        {
+            var TaskListRowContext = context.GetContextItem<TaskListRowContext>();
+
+            var content = await output.GetChildContentAsync();
+
+            TaskListRowContext.AddAction(new TaskListRowAction()
+            {
+                Attributes = output.Attributes.ToAttributeDictionary(),
+                Content = content.Snapshot(),
+                VisuallyHiddenText = VisuallyHiddenText
+            });
+
+            output.SuppressOutput();
+        }
+    }
+}

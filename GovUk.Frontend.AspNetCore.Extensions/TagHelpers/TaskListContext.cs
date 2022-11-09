a7921e0709f8d9f79824d349df0536704c2cafc0
Samuel Hägglund <107886283+samuelhaa@users.noreply.github.com>
2022-11-09 16:41:21 +0000

    Add class TaskListContext

---
+++ GovUk.Frontend.AspNetCore.Extensions/TagHelpers/TaskListContext.cs
@@ -0,0 +1,24 @@
+﻿using GovUk.Frontend.AspNetCore.HtmlGeneration;
+using System.Collections.Generic;
+
+namespace GovUk.Frontend.AspNetCore.Extensions.TagHelpers
+{
+    internal class TaskListContext
+    {
+        private readonly List<TaskListRow> _rows;
+
+        public TaskListContext()
+        {
+            _rows = new List<TaskListRow>();
+        }
+
+        public IReadOnlyList<TaskListRow> Rows => _rows;
+
+        public void AddRow(TaskListRow row)
+        {
+            Guard.ArgumentNotNull(nameof(row), row);
+
+            _rows.Add(row);
+        }
+    }
+}

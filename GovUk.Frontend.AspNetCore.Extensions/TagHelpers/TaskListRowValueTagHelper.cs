f0144c84e932aa4fb676e91c8b191a2e33b89144
Samuel Hägglund <107886283+samuelhaa@users.noreply.github.com>
2022-11-09 16:20:35 +0000

    Add class TaskListRowValueTagHelper

---
+++ GovUk.Frontend.AspNetCore.Extensions/TagHelpers/TaskListRowValueTagHelper.cs
@@ -0,0 +1,27 @@
+﻿using Microsoft.AspNetCore.Razor.TagHelpers;
+using System.Threading.Tasks;
+
+namespace GovUk.Frontend.AspNetCore.Extensions.TagHelpers
+{
+    /// <summary>
+    /// Represents the value in a GDS task list component row.
+    /// </summary>
+    [HtmlTargetElement(TagName, ParentTag = TaskListRowTagHelper.TagName)]
+    [OutputElementHint(ComponentGenerator.TaskListRowValueElement)]
+    public class TaskListRowValueTagHelper : TagHelper
+    {
+        internal const string TagName = "govuk-task-list-row-value";
+
+        /// <inheritdoc/>
+        public override async Task ProcessAsync(TagHelperContext context, TagHelperOutput output)
+        {
+            var TaskListRowContext = context.GetContextItem<TaskListRowContext>();
+
+            var content = await output.GetChildContentAsync();
+
+            TaskListRowContext.SetValue(output.Attributes.ToAttributeDictionary(), content.Snapshot());
+
+            output.SuppressOutput();
+        }
+    }
+}

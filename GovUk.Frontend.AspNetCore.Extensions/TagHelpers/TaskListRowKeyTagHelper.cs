5f9c23ba6fe08fdfde717529d69ccdb395ab2f8c
Samuel Hägglund <107886283+samuelhaa@users.noreply.github.com>
2022-11-09 16:27:35 +0000

    Add class TaskListRowKeyTagHelper

---
+++ GovUk.Frontend.AspNetCore.Extensions/TagHelpers/TaskListRowKeyTagHelper.cs
@@ -0,0 +1,28 @@
+﻿using GovUk.Frontend.AspNetCore.TagHelpers;
+using Microsoft.AspNetCore.Razor.TagHelpers;
+using System.Threading.Tasks;
+
+namespace GovUk.Frontend.AspNetCore.Extensions.TagHelpers
+{
+    /// <summary>
+    /// Represents the value in a GDS task list component row.
+    /// </summary>
+    [HtmlTargetElement(TagName, ParentTag = TaskListRowTagHelper.TagName)]
+    [OutputElementHint(ComponentGenerator.TaskListRowKeyElement)]
+    public class TaskListRowKeyTagHelper : TagHelper
+    {
+        internal const string TagName = "govuk-task-list-row-key";
+
+        /// <inheritdoc/>
+        public override async Task ProcessAsync(TagHelperContext context, TagHelperOutput output)
+        {
+            var TaskListRowContext = context.GetContextItem<TaskListRowContext>();
+
+            var content = await output.GetChildContentAsync();
+
+            TaskListRowContext.SetKey(output.Attributes.ToAttributeDictionary(), content.Snapshot());
+
+            output.SuppressOutput();
+        }
+    }
+}

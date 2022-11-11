d5176f7d85fdc324cd56d2059b35b3e4b4b61ddc
Rick Mason <rick.mason@tpr.gov.uk>
2022-11-11 16:29:42 +0000

    'Back to top' should appear only when the content is scrolled to 4x the viewport height AB#143794

---
+++ GovUk.Frontend.AspNetCore.Extensions/wwwroot/tpr/tpr-back-to-top.js
@@ -0,0 +1,12 @@
+﻿// Back to top should only appear when the page is longer than 4vh
+document.addEventListener('scroll', function () {
+    const backToTopScrollPosition = 4 * window.innerHeight;
+    const y = document.getElementsByTagName("html")[0].scrollTop;
+    if (y > backToTopScrollPosition) {
+
+        document.querySelector('.tpr-back-to-top').style.display= 'block';
+
+    } else {
+        document.querySelector('.tpr-back-to-top').style.display = 'none';
+    }
+});

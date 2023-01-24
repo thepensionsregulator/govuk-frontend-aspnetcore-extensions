f77cff7385d4969372b77f5571c2d626fe746497
HarryAmmon <harry.ammon@tpr.gov.uk>
2023-01-24 11:10:28 +0000

    #148487 add JS to hide banner

---
+++ GovUk.Frontend.Umbraco.ExampleApp/wwwroot/js/NotificationBanner.js
@@ -0,0 +1,11 @@
+﻿window.addEventListener("DOMContentLoaded", function () {
+    const dismissButton = document.getElementsByClassName("dismiss")[0];
+    dismissButton.addEventListener("click", dismissBanner);
+
+    console.log("hello world agagin");
+
+    function dismissBanner() {
+        const banner = document.getElementsByClassName("tpr-notification-banner")[0];
+        banner.style.display = "none";
+    }
+});

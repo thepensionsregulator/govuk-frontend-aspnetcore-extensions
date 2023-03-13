56020568eaf63325334768672f32effcae5f9cd2
Rick Mason <rick.mason@tpr.gov.uk>
2023-03-13 17:56:03 +0000

    It should be possible to disable a button in Umbraco, and this should be done using the aria-disabled rather than the disabled attribute AB#151932

---
+++ GovUk.Frontend.Umbraco.ExampleApp/Controllers/ButtonController.cs
@@ -0,0 +1,28 @@
+﻿using GovUk.Frontend.AspNetCore.Extensions.Validation;
+using GovUk.Frontend.Umbraco.ExampleApp.Models;
+using Microsoft.AspNetCore.Mvc;
+using Microsoft.AspNetCore.Mvc.ViewEngines;
+using Microsoft.Extensions.Logging;
+using Umbraco.Cms.Core.Web;
+using Umbraco.Cms.Web.Common.Controllers;
+using Umbraco.Cms.Web.Common.PublishedModels;
+
+namespace GovUk.Frontend.Umbraco.ExampleApp.Controllers
+{
+    public class ButtonController : RenderController
+    {
+        public ButtonController(ILogger<RenderController> logger, ICompositeViewEngine compositeViewEngine, IUmbracoContextAccessor umbracoContextAccessor) : base(logger, compositeViewEngine, umbracoContextAccessor)
+        {
+        }
+
+        [ModelType(typeof(ButtonViewModel))]
+        public override IActionResult Index()
+        {
+            var viewModel = new ButtonViewModel
+            {
+                Page = new Button(CurrentPage, null)
+            };
+            return CurrentTemplate(viewModel);
+        }
+    }
+}
---
+++ GovUk.Frontend.Umbraco.ExampleApp/Models/ButtonViewModel.cs
@@ -0,0 +1,9 @@
+﻿using Umbraco.Cms.Web.Common.PublishedModels;
+
+namespace GovUk.Frontend.Umbraco.ExampleApp.Models
+{
+	public class ButtonViewModel
+	{
+		public Button? Page { get; set; }
+	}
+}

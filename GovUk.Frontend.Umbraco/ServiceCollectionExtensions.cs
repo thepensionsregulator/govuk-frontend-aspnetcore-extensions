0cc2c681745069fa401292a71dbd331a56ed847c
Rick Mason <rick.mason@tpr.gov.uk>
2023-06-29 14:40:58 +0100

    Commit to rebase

--- GovUk.Frontend.Umbraco/ServiceCollectionExtensions.cs
+++ GovUk.Frontend.Umbraco/ServiceCollectionExtensions.cs
@@ -2,6 +2,7 @@ using GovUk.Frontend.AspNetCore;
 using GovUk.Frontend.AspNetCore.Extensions;
 using GovUk.Frontend.Umbraco.ModelBinding;
 using GovUk.Frontend.Umbraco.Services;
+using GovUk.Frontend.Umbraco.Validation;
 using Microsoft.Extensions.DependencyInjection;
 using System;
 using System.Linq;
@@ -32,7 +33,7 @@ namespace GovUk.Frontend.Umbraco
             services.AddTransient<IUmbracoPaginationFactory, UmbracoPaginationFactory>();
             services.AddMvc(options =>
             {
-                //options.Filters.Add<RemoveBlockListSettingsErrorsActionFilter>();
+                options.Filters.Add<RemoveBlockListSettingsErrorsActionFilter>();
                 // Replace the custom date model binder from the base project with a copy that has
                 // been modified to make the error messages configurable in Umbraco
                 var govukDateBinder = options.ModelBinderProviders.FirstOrDefault(x => x.GetType().FullName == "GovUk.Frontend.AspNetCore.ModelBinding.DateInputModelBinderProvider");

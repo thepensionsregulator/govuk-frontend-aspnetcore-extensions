613aa70df602290a44551803b71f60a0e579d083
Samuel Hägglund <107886283+samuelhaa@users.noreply.github.com>
2023-06-29 10:51:43 +0100

    Add companies house validation attribute
    A valid companies house should consist of
    
    - 8 numbers or
    - 2 letters and 6 numbers or
    - 1 letter and 7 numbers
    
    AB#155184

---
+++ GovUk.Frontend.AspNetCore.Extensions.Tests/IsCompaniesHouseNumberAttributeTest.cs
@@ -0,0 +1,56 @@
+﻿using GovUk.Frontend.AspNetCore.Extensions.Validation;
+using NUnit.Framework;
+
+namespace GovUk.Frontend.AspNetCore.Extensions.Tests
+{
+    public class IsCompaniesHouseNumberAttributeTest
+    {
+        [Test]
+        [TestCase("AA123456")]
+        [TestCase("B1234567")]
+        [TestCase("12345678")]
+        public void Valid_companies_house_number(string? input)
+        {
+            TestNumber(input, expected: true);
+        }
+
+        [Test]
+        [TestCase("AA1234560")]
+        [TestCase("B12345670")]
+        [TestCase("123456780")]
+        public void Numbers_are_too_long(string number)
+        {
+            TestNumber(number, expected: false);
+        }
+
+        [Test]
+        [TestCase("AA12345")]
+        [TestCase("B123456")]
+        [TestCase("1234567")]
+        public void NumbersAreTooShort(string? number)
+        {
+            TestNumber(number, expected: false);
+        }
+
+        [Test]
+        [TestCase("!A12345")]
+        [TestCase("AAA12345")]
+        [TestCase("AAA1234")]
+        public void DoesNotStartWithCorrectLetters(string? number)
+        {
+            TestNumber(number, expected: false);
+        }
+
+        private void TestNumber(string? input, bool expected)
+        {
+            // Arrange
+            var attribute = new IsCompaniesHouseNumberAttribute();
+
+            // Act
+            var result = attribute.IsValid(input);
+
+            // Assert
+            Assert.That(result, Is.EqualTo(expected));
+        }
+    }
+}

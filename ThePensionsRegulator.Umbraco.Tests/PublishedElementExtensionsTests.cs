2a0882af9b21c582747d7658fcc4608365a2cec9
Rick Mason <rick.mason@tpr.gov.uk>
2023-08-11 19:02:24 +0100

    Add extension method to convert an IPublishedElement to its ModelsBuilder model

---
+++ ThePensionsRegulator.Umbraco.Tests/PublishedElementExtensionsTests.cs
@@ -0,0 +1,24 @@
+﻿using Moq;
+using ThePensionsRegulator.Umbraco.BlockLists;
+using Umbraco.Cms.Core.Models.PublishedContent;
+using Umbraco.Cms.Web.Common.PublishedModels;
+
+namespace ThePensionsRegulator.Umbraco.Tests
+{
+	public class PublishedElementExtensionsTests
+	{
+		[Test]
+		public void IPublishedElement_is_converted_to_ModelsBuilder_model()
+		{
+			var contentType = new Mock<IPublishedContentType>();
+			contentType.Setup(x => x.Alias).Returns("exampleModelsBuilderModel");
+
+			var blockContent = new Mock<IPublishedElement>();
+			blockContent.SetupGet(x => x.ContentType).Returns(contentType.Object);
+
+			var result = blockContent.Object.AsPublishedElementModel<ExampleModelsBuilderModel>();
+
+			Assert.That(result, Is.Not.Null);
+		}
+	}
+}

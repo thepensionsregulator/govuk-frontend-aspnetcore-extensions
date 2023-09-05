7f485219137d3a0c2ae4de516e1845d90621f136
Rick Mason <rick.mason@tpr.gov.uk>
2023-09-05 09:54:25 +0100

    Add custom backoffice views for the block grid editor. Rich text block preview should use GOV.UK styles. Image preview should be in TPR project.

---
+++ GovUk.Frontend.Umbraco/App_Plugins/GOVUK/blocks/controllers/govukPageHeading.controller.js
@@ -0,0 +1,3 @@
+﻿angular.module("umbraco").controller("govukPageHeadingController", function ($scope, editorState) {
+    $scope.pageHeading = editorState.current.variants.filter(x => x.active)[0].name;
+});

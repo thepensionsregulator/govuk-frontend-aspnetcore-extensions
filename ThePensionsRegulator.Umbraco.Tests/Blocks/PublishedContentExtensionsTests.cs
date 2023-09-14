1e7dd901404e0842b56a05877816ae9d47eb969e
Rick Mason <rick.mason@tpr.gov.uk>
2023-09-14 17:46:09 +0100

    Review extension methods for finding blocks to be compatible with both block list and block grid

--- ThePensionsRegulator.Umbraco.Tests/Blocks/PublishedContentExtensionsTests.cs
+++ ThePensionsRegulator.Umbraco.Tests/Blocks/PublishedContentExtensionsTests.cs
@@ -1,6 +1,5 @@
 ﻿using ThePensionsRegulator.Umbraco.Blocks;
 using ThePensionsRegulator.Umbraco.Testing;
-using Umbraco.Cms.Core.Models.Blocks;
 using Umbraco.Cms.Core.Models.PublishedContent;
 
 namespace ThePensionsRegulator.Umbraco.Tests.Blocks
@@ -9,64 +8,36 @@ namespace ThePensionsRegulator.Umbraco.Tests.Blocks
     public class PublishedContentExtensionsTests
     {
         [Test]
-        public void Finds_multiple_block_lists()
+        public void Finds_multiple_mixed_block_lists_and_grids()
         {
             // Arrange
-            var blockList1 = new BlockListModel(Array.Empty<BlockListItem>());
-            var blockList2 = new BlockListModel(Array.Empty<BlockListItem>());
+            var blockList1 = new OverridableBlockListModel(new[] { UmbracoBlockListFactory.CreateOverridableBlock(UmbracoBlockListFactory.CreateContentOrSettings("alias").Object) });
+            var blockList2 = new OverridableBlockListModel(new[] { UmbracoBlockListFactory.CreateOverridableBlock(UmbracoBlockListFactory.CreateContentOrSettings("alias").Object) });
+            var blockGrid1 = new OverridableBlockGridModel(new[] { UmbracoBlockGridFactory.CreateOverridableBlock(UmbracoBlockGridFactory.CreateContentOrSettings("alias").Object) });
+            var blockGrid2 = new OverridableBlockGridModel(new[] { UmbracoBlockGridFactory.CreateOverridableBlock(UmbracoBlockGridFactory.CreateContentOrSettings("alias").Object) });
 
             var content = UmbracoContentFactory.CreateContent<IPublishedContent>();
             content.SetupUmbracoBlockListPropertyValue("blockList1", blockList1);
             content.SetupUmbracoBlockListPropertyValue("blockList2", blockList2);
-
-            // Act
-            var results = content.Object.FindBlockLists().ToList();
-
-            // Assert
-            Assert.That(results.Count, Is.EqualTo(2));
-            Assert.Contains(blockList1, results);
-            Assert.Contains(blockList2, results);
-        }
-
-        [Test]
-        public void Finds_multiple_block_grids()
-        {
-            // Arrange
-            var blockGrid1 = new BlockGridModel(Array.Empty<BlockGridItem>(), 1);
-            var blockGrid2 = new BlockGridModel(Array.Empty<BlockGridItem>(), 1);
-
-            var content = UmbracoContentFactory.CreateContent<IPublishedContent>();
             content.SetupUmbracoBlockGridPropertyValue("blockGrid1", blockGrid1);
             content.SetupUmbracoBlockGridPropertyValue("blockGrid2", blockGrid2);
 
             // Act
-            var results = content.Object.FindBlockGrids().ToList();
+            var results = content.Object.FindOverridableBlockModels(null).ToList();
 
             // Assert
-            Assert.That(results.Count, Is.EqualTo(2));
-            Assert.Contains(blockGrid1, results);
-            Assert.Contains(blockGrid2, results);
-        }
-
-
-        [Test]
-        public void Finds_mixed_block_lists_and_grids()
-        {
-            // Arrange
-            var blockList = new BlockListModel(Array.Empty<BlockListItem>());
-            var blockGrid = new BlockGridModel(Array.Empty<BlockGridItem>(), 1);
-
-            var content = UmbracoContentFactory.CreateContent<IPublishedContent>();
-            content.SetupUmbracoBlockListPropertyValue("blockList", blockList);
-            content.SetupUmbracoBlockGridPropertyValue("blockGrid", blockGrid);
-
-            // Act
-            var results = content.Object.FindBlockModelCollections(null).ToList();
-
-            // Assert
-            Assert.That(results.Count, Is.EqualTo(2));
-            Assert.Contains(blockList, results);
-            Assert.Contains(blockGrid, results);
+            Assert.Multiple(() =>
+            {
+                Assert.That(results.Count, Is.EqualTo(4));
+                Assert.Contains(blockList1, results);
+                Assert.Contains(blockList2, results);
+                Assert.Contains(blockGrid1, results);
+                Assert.Contains(blockGrid2, results);
+                Assert.That(results[0].Count, Is.EqualTo(1));
+                Assert.That(results[1].Count, Is.EqualTo(1));
+                Assert.That(results[2].Count, Is.EqualTo(1));
+                Assert.That(results[3].Count, Is.EqualTo(1));
+            });
         }
     }
 }

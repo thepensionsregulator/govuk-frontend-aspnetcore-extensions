c29fe49c80570bee43e52dc4d6f7268fba7b87cd
Rick Mason <rick.mason@tpr.gov.uk>
2023-10-02 14:27:18 +0100

    The page heading should be overridable AB#168435

--- GovUk.Frontend.Umbraco.Tests/Blocks/ExampleModelsBuilderModel.generated.cs
+++ GovUk.Frontend.Umbraco.Tests/Blocks/ExampleModelsBuilderModel.generated.cs
@@ -18,14 +18,14 @@ using Umbraco.Extensions;
 
 namespace Umbraco.Cms.Web.Common.PublishedModels
 {
-	/// <summary>Page heading</summary>
-	[PublishedModel("govukPageHeading")]
-	public partial class GovukPageHeading : PublishedElementModel
+    /// <summary>ExampleModelsBuilderModel</summary>
+    [PublishedModel("exampleModelsBuilderModel")]
+	public partial class ExampleModelsBuilderModel : PublishedContentModel
 	{
 		// helpers
 #pragma warning disable 0109 // new is redundant
 		[global::System.CodeDom.Compiler.GeneratedCodeAttribute("Umbraco.ModelsBuilder.Embedded", "10.6.1+82eae48")]
-		public new const string ModelTypeAlias = "govukPageHeading";
+		public new const string ModelTypeAlias = "exampleModelsBuilderModel";
 		[global::System.CodeDom.Compiler.GeneratedCodeAttribute("Umbraco.ModelsBuilder.Embedded", "10.6.1+82eae48")]
 		public new const PublishedItemType ModelItemType = PublishedItemType.Content;
 		[global::System.CodeDom.Compiler.GeneratedCodeAttribute("Umbraco.ModelsBuilder.Embedded", "10.6.1+82eae48")]
@@ -34,14 +34,14 @@ namespace Umbraco.Cms.Web.Common.PublishedModels
 			=> PublishedModelUtility.GetModelContentType(publishedSnapshotAccessor, ModelItemType, ModelTypeAlias);
 		[global::System.CodeDom.Compiler.GeneratedCodeAttribute("Umbraco.ModelsBuilder.Embedded", "10.6.1+82eae48")]
 		[return: global::System.Diagnostics.CodeAnalysis.MaybeNull]
-		public static IPublishedPropertyType GetModelPropertyType<TValue>(IPublishedSnapshotAccessor publishedSnapshotAccessor, Expression<Func<GovukPageHeading, TValue>> selector)
+		public static IPublishedPropertyType GetModelPropertyType<TValue>(IPublishedSnapshotAccessor publishedSnapshotAccessor, Expression<Func<ExampleModelsBuilderModel, TValue>> selector)
 			=> PublishedModelUtility.GetModelPropertyType(GetModelContentType(publishedSnapshotAccessor), selector);
 #pragma warning restore 0109
 
 		private IPublishedValueFallback _publishedValueFallback;
 
 		// ctor
-		public GovukPageHeading(IPublishedElement content, IPublishedValueFallback publishedValueFallback)
+		public ExampleModelsBuilderModel(IPublishedContent content, IPublishedValueFallback publishedValueFallback)
 			: base(content, publishedValueFallback)
 		{
 			_publishedValueFallback = publishedValueFallback;
@@ -50,11 +50,11 @@ namespace Umbraco.Cms.Web.Common.PublishedModels
 		// properties
 
 		///<summary>
-		/// Text: Defaults to the name of the page if left blank.
+		/// BlockList
 		///</summary>
-		[global::System.CodeDom.Compiler.GeneratedCodeAttribute("Umbraco.ModelsBuilder.Embedded", "10.4.2+c5fe779")]
+		[global::System.CodeDom.Compiler.GeneratedCodeAttribute("Umbraco.ModelsBuilder.Embedded", "10.6.1+82eae48")]
 		[global::System.Diagnostics.CodeAnalysis.MaybeNull]
-		[ImplementPropertyType("text")]
-		public virtual string Text => this.Value<string>(_publishedValueFallback, "text");
-	}
+		[ImplementPropertyType("blockList")]
+		public virtual global::ThePensionsRegulator.Umbraco.Blocks.OverridableBlockListModel BlockList => this.Value<global::ThePensionsRegulator.Umbraco.Blocks.OverridableBlockListModel>(_publishedValueFallback, "blockList");
+    }
 }
---
+++ GovUk.Frontend.Umbraco/Blocks/PublishedContentModelExtensions.cs
@@ -0,0 +1,25 @@
+﻿using ThePensionsRegulator.Umbraco.Blocks;
+using Umbraco.Cms.Core.Models.PublishedContent;
+
+namespace GovUk.Frontend.Umbraco.Blocks
+{
+	public static class PublishedContentModelExtensions
+	{
+		/// <summary>
+		/// Gets the page title from a page heading block if present, otherwise from the current content node name.
+		/// </summary>
+		/// <param name="content">The model for the current content node.</param>
+		/// <returns>A page title.</returns>
+		public static string? PageHeadingOrName(this PublishedContentModel content)
+		{
+			var pageHeading = (OverridableBlockListItem?)content.FindBlockLists().FindBlockByContentTypeAlias(ElementTypeAliases.PageHeading);
+			if (pageHeading is not null)
+			{
+				var text = pageHeading.Content.Value<string>(PropertyAliases.PageHeading);
+				if (!string.IsNullOrWhiteSpace(text)) { return text; }
+			}
+
+			return content.Name;
+		}
+	}
+}

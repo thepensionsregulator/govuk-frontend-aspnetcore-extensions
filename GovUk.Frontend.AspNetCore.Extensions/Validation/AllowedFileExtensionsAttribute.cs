b2c3f93492839edf6cc8e9275e0bd7bdc326f235
Joel Godden <Joel.Godden@tpr.gov.uk>
2024-04-16 16:48:15 +0100

    WIP set up attributes

---
+++ GovUk.Frontend.AspNetCore.Extensions/Validation/AllowedFileExtensionsAttribute.cs
@@ -0,0 +1,34 @@
+﻿using Microsoft.AspNetCore.Http;
+using System.ComponentModel.DataAnnotations;
+using System.IO;
+using System.Linq;
+
+namespace GovUk.Frontend.AspNetCore.Extensions.Validation
+{
+    public class AllowedExtensionsAttribute : ValidationAttribute
+    {
+        private readonly string[] _extensions;
+        public AllowedExtensionsAttribute(string[] extensions)
+        {
+            _extensions = extensions;
+        }
+
+        protected override ValidationResult IsValid(
+        object value, ValidationContext validationContext)
+        {
+            var file = value as IFormFile;
+            if (file is not null)
+            {
+                var extension = Path.GetExtension(file.FileName);
+                if (!_extensions.Contains(extension.ToLower()))
+                {
+                    return new ValidationResult(GetErrorMessage());
+                }
+            }
+
+            return ValidationResult.Success;
+        }
+
+        public string GetErrorMessage() => $"Please upload one of the following file types: {string.Join(',', _extensions)}";  
+    }
+}
---
+++ GovUk.Frontend.AspNetCore.Extensions/Validation/MaxFileSizeAttribute.cs
@@ -0,0 +1,33 @@
+﻿using Microsoft.AspNetCore.Http;
+using System.ComponentModel.DataAnnotations;
+
+
+namespace GovUk.Frontend.AspNetCore.Extensions.Validation
+{
+    public class MaxFileSizeAttribute : ValidationAttribute
+    {
+        private readonly int _maxFileSize;
+        public MaxFileSizeAttribute(int maxFileSize)
+        {
+            _maxFileSize = maxFileSize;
+        }
+
+        protected override ValidationResult IsValid(
+        object value, ValidationContext validationContext)
+        {
+            var file = value as IFormFile;
+            if (file is not null)
+            {
+                if (file.Length > _maxFileSize)
+                {
+                    return new ValidationResult(GetErrorMessage());
+                }
+            }
+
+            return ValidationResult.Success;
+        }
+
+        public string GetErrorMessage() => $"Maximum allowed file size is {_maxFileSize} bytes.";
+        
+    }
+}

f4d737cbc096ff13342378a07e47756e6ec31a73
Joel Godden <Joel.Godden@tpr.gov.uk>
2024-04-18 17:40:24 +0100

    WIP make a start o osme unit tests

---
+++ GovUk.Frontend.AspNetCore.Extensions.Tests/AllowedFileExtensionsAttributeTests.cs
@@ -0,0 +1,30 @@
+﻿using GovUk.Frontend.AspNetCore.Extensions.Validation;
+using Microsoft.AspNetCore.Http;
+using NUnit.Framework;
+using System;
+using System.Collections.Generic;
+
+namespace GovUk.Frontend.AspNetCore.Extensions.Tests
+{
+    [TestFixture]
+    public class AllowedFileExtensionsAttributeTests
+    {
+
+        [Test]
+        [TestCaseSource(nameof(TestExcelFiles))]
+        public void Should_do_the_thing(IFormFile file)
+        {
+            var sut = new AllowedFileExtensionsAttribute(new[] { ".xcls" });
+
+            // Act
+            var result = sut.IsValid(file);
+
+            // Assert
+            Assert.AreEqual(result, true);
+        }
+
+
+        private static IEnumerable<IFormFile> TestExcelFiles;
+
+    }
+}

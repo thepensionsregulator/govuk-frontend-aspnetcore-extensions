607384c8667c8c282257aa1805815aa87959732c
Joel Godden <Joel.Godden@tpr.gov.uk>
2024-04-22 12:26:26 +0100

    more tests

--- GovUk.Frontend.AspNetCore.Extensions.Tests/AllowedFileExtensionsAttributeTests.cs
+++ GovUk.Frontend.AspNetCore.Extensions.Tests/AllowedFileExtensionsAttributeTests.cs
@@ -14,13 +14,29 @@ namespace GovUk.Frontend.AspNetCore.Extensions.Tests
 
         [Test]
         [TestCaseSource(nameof(TestExcelFiles))]
-        public void Should_ReturnValidResult_WhenFilesMatchFileTypesSpecified(IFormFile file)
+        public void Should_ReturnValidResult_WhenFileMatchesFileTypesSpecified(IFormFile file)
         {
             var sut = new AllowedFileExtensionsAttribute([".xls", ".xlsx", ".xlsm"]);
             var result = sut.IsValid(file);
             Assert.AreEqual(result, true);
         }
 
+        [Test]
+        public void Should_ReturnNotValidResult_WhenFileDoesNotMatchFileTypesSpecified()
+        {
+            var file = CreateFormFile("test.pdf");
+            var sut = new AllowedFileExtensionsAttribute([".xls", ".xlsx", ".xlsm"]);
+            var result = sut.IsValid(file);
+            Assert.AreEqual(result, false);
+        }
+
+        [Test]
+        public void Should_ThrowException_IfTargetPropertyNotIFormFile()
+        {
+            var testValue = "Some test string property value";
+            var sut = new AllowedFileExtensionsAttribute([".pdf"]);
+            Assert.Throws<InvalidOperationException>(() => sut.IsValid(testValue));
+        }
 
         private static IEnumerable<IFormFile> TestExcelFiles() =>
             new List<IFormFile>
@@ -35,6 +51,5 @@ namespace GovUk.Frontend.AspNetCore.Extensions.Tests
             var bytes = Encoding.UTF8.GetBytes("This is a dummy file");
             return new FormFile(new MemoryStream(bytes), 0, bytes.Length, "Data", filename);
         }
-
     }
 }

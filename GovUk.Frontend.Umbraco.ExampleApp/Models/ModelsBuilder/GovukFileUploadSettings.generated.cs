d79186abd0bfe6132e6ad3f939a401f3818834e5
Joel Godden <Joel.Godden@tpr.gov.uk>
2024-05-14 13:00:40 +0100

    modelsbuilder update

--- GovUk.Frontend.Umbraco.ExampleApp/Models/ModelsBuilder/GovukFileUploadSettings.generated.cs
+++ GovUk.Frontend.Umbraco.ExampleApp/Models/ModelsBuilder/GovukFileUploadSettings.generated.cs
@@ -50,6 +50,22 @@ namespace Umbraco.Cms.Web.Common.PublishedModels
 		// properties
 
 		///<summary>
+		/// Allowed file types: Sets the message displayed if the file uploaded is not one of the supported types/extensions
+		///</summary>
+		[global::System.CodeDom.Compiler.GeneratedCodeAttribute("Umbraco.ModelsBuilder.Embedded", "13.2.0+7dff3a3")]
+		[global::System.Diagnostics.CodeAnalysis.MaybeNull]
+		[ImplementPropertyType("errorMessageAllowedFileExtensions")]
+		public virtual string ErrorMessageAllowedFileExtensions => this.Value<string>(_publishedValueFallback, "errorMessageAllowedFileExtensions");
+
+		///<summary>
+		/// Maximum file size: Sets the message displayed if the file uploaded exceeds the maximum size
+		///</summary>
+		[global::System.CodeDom.Compiler.GeneratedCodeAttribute("Umbraco.ModelsBuilder.Embedded", "13.2.0+7dff3a3")]
+		[global::System.Diagnostics.CodeAnalysis.MaybeNull]
+		[ImplementPropertyType("errorMessageMaxFileSize")]
+		public virtual string ErrorMessageMaxFileSize => this.Value<string>(_publishedValueFallback, "errorMessageMaxFileSize");
+
+		///<summary>
 		/// File types: A comma-separated list of accepted file extensions or media types. See [the accept attribute on MDN](https://developer.mozilla.org/en-US/docs/Web/HTML/Attributes/accept).
 		///</summary>
 		[global::System.CodeDom.Compiler.GeneratedCodeAttribute("Umbraco.ModelsBuilder.Embedded", "13.2.0+7dff3a3")]

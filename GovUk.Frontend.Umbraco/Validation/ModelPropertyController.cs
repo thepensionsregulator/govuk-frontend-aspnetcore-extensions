00526ffbd7262c14b9addb501619ea16b4808815
HarryAmmon <harry.ammon@tpr.gov.uk>
2024-05-23 15:16:14 +0100

    Reduce the list of dlls to load as something was causing this method to throw an exception in the scheme return project

--- GovUk.Frontend.Umbraco/Validation/ModelPropertyController.cs
+++ GovUk.Frontend.Umbraco/Validation/ModelPropertyController.cs
@@ -1,5 +1,6 @@
 ﻿using GovUk.Frontend.AspNetCore.Extensions.Validation;
 using Microsoft.AspNetCore.Mvc;
+using Microsoft.Extensions.Logging;
 using System;
 using System.Collections.Generic;
 using System.IO;
@@ -10,20 +11,34 @@ using Umbraco.Cms.Core.Models.PublishedContent;
 using Umbraco.Cms.Web.BackOffice.Controllers;
 using Umbraco.Cms.Web.Common.Attributes;
 using Umbraco.Cms.Web.Common.Controllers;
+using Umbraco.Extensions;
 
 namespace GovUk.Frontend.Umbraco.Validation
 {
     [PluginController("GOVUK")]
     public class ModelPropertyController : UmbracoAuthorizedApiController
     {
+        private readonly ILogger<ModelPropertyController> _logger;
+
+        public ModelPropertyController(ILogger<ModelPropertyController> logger)
+        {
+            _logger = logger;
+        }
+
+
         [HttpGet]
         public IEnumerable<string> ForDocumentType(string alias)
         {
             var filepath = Path.GetDirectoryName(Assembly.GetExecutingAssembly().Location)!;
+           
             List<Type> controllers = new();
-            foreach (string assemblyFile in Directory.GetFiles(filepath, "*.dll"))
-            {
-                Assembly assembly = Assembly.LoadFrom(assemblyFile);
+
+            var files = Directory.GetFiles(filepath, "*.dll").ToList();
+            files.RemoveAll(x => x.Contains("System") || x.Contains("Microsoft")); // Don't load dlls where there won't be any custom code. This list is not exhaustive
+
+            foreach(var file in files) 
+            { 
+                Assembly assembly = Assembly.LoadFrom(file);
                 List<Type> controllersToAdd = assembly.GetTypes().Where(x => x.IsSubclassOf(typeof(RenderController))).ToList();
                 controllers.AddRange(controllersToAdd);
             }

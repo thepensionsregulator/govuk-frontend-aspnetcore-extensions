c18a0b96970135cf44c644fd167012f07a35768d
Samuel Hägglund <107886283+samuelhaa@users.noreply.github.com>
2024-08-01 13:16:54 +0100

    Add support for Pdf

---
+++ GovUk.Frontend.AspNetCore.Extensions.Tests/BinaryFileValidators/FileTypes/PdfTest.cs
@@ -0,0 +1,32 @@
+﻿using GovUk.Frontend.AspNetCore.Extensions.Validation.BinaryFileValidators.FileTypes;
+using NUnit.Framework;
+using System.Collections;
+using System.IO;
+
+namespace GovUk.Frontend.AspNetCore.Extensions.Tests.BinaryFileValidators.FileTypes
+{
+    internal class PdfTest
+    {
+        private class PdfDataProvider : IEnumerable
+        {
+            public IEnumerator GetEnumerator()
+            {
+                yield return new object[] { TestFileLocator.WordSavedAsPdf, true };
+            }
+        }
+
+        [TestCaseSource(typeof(PdfDataProvider))]
+        public void Test(string filename, bool expected)
+        {
+            var bytes = File.ReadAllBytes(filename);
+
+            var memoryStream = new MemoryStream(bytes);
+            var pdf = new Pdf();
+            var valid = pdf.IsMatch(memoryStream, filename);
+
+            memoryStream.Dispose();
+
+            Assert.That(valid, Is.EqualTo(expected));
+        }
+    }
+}
---
+++ GovUk.Frontend.AspNetCore.Extensions/Validation/BinaryFileValidators/FileTypes/Pdf.cs
@@ -0,0 +1,57 @@
+﻿using System.IO;
+using System.Security.Cryptography.Xml;
+
+namespace GovUk.Frontend.AspNetCore.Extensions.Validation.BinaryFileValidators.FileTypes
+{
+    /// <summary>
+    /// Specifies the format of a Portable Document Format (PDF) file.
+    /// </summary>
+    public class Pdf : FileType, IFileTypeValidator
+    {
+        private const uint MaxFileHeaderSize = 1024;
+
+        public Pdf() : this([0x25, 0x50, 0x44, 0x46])
+        {
+        }
+
+        protected Pdf(byte[] signature) : base(signature, "application/pdf", [".pdf"], 0)
+        {
+        }
+
+        public override bool IsMatch(Stream stream)
+        {
+            if (stream == null || stream.Length < HeaderLength)
+            {
+                return false;
+            }
+
+            stream.Position = 0;
+            var signatureValidationIndex = 0;
+            int fileByte;
+
+            while (stream.Position < MaxFileHeaderSize && (fileByte = stream.ReadByte()) != -1)
+            {
+                if (CompareFileByteToSignatureAt((byte)fileByte, signatureValidationIndex))
+                {
+                    signatureValidationIndex++;
+                }
+                else
+                {
+                    signatureValidationIndex = 0;
+                }
+
+                if (signatureValidationIndex == Signature.Count)
+                {
+                    return true;
+                }
+            }
+
+            return false;
+        }
+
+        protected virtual bool CompareFileByteToSignatureAt(byte fileByte, int signatureIndex)
+        {
+            return fileByte == Signature[signatureIndex];
+        }
+    }
+}

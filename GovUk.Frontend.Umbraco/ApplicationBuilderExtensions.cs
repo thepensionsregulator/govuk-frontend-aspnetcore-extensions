43c88f23ed38517900e6e53fe2b62013553051dc
Samuel Hägglund <107886283+samuelhaa@users.noreply.github.com>
2024-08-02 10:50:22 +0100

    Add AllowedFileTypes to PropertyAliases

--- GovUk.Frontend.Umbraco/ApplicationBuilderExtensions.cs
+++ GovUk.Frontend.Umbraco/ApplicationBuilderExtensions.cs
@@ -35,7 +35,7 @@ namespace GovUk.Frontend.Umbraco
                 { typeof(RangeAttribute), PropertyAliases.ErrorMessageRange },
                 { typeof(DateRangeAttribute), PropertyAliases.ErrorMessageRange },
                 { typeof(CompareAttribute), PropertyAliases.ErrorMessageCompare },
-                { typeof(AllowedFileTypesAttribute), PropertyAliases.ErrorMessageAllowedFileExtensions },
+                { typeof(AllowedFileTypesAttribute), PropertyAliases.ErrorMessageAllowedFileTypes },
                 { typeof(MaxFileSizeAttribute), PropertyAliases.ErrorMessageMaxFileSize }
             }));
 
@@ -51,7 +51,6 @@ namespace GovUk.Frontend.Umbraco
                   "/_content/ThePensionsRegulator.GovUk.Frontend/lib/jquery-validation/dist/jquery.validate.min.js",
                   "/_content/ThePensionsRegulator.GovUk.Frontend/govuk/govuk-validation.js",
                   "/_content/ThePensionsRegulator.GovUk.Frontend/lib/jquery-validation-unobtrusive/jquery.validate.unobtrusive.min.js");
-
             });
 
             return app;

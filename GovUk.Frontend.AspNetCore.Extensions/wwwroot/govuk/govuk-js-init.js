93a239e2200d1f25c2686bfdb839ef2b50c374a2
Rick Mason <rick.mason@tpr.gov.uk>
2024-09-30 12:11:31 +0100

    Demo how to support Welsh localisation of character count AB#206433

--- GovUk.Frontend.AspNetCore.Extensions/wwwroot/govuk/govuk-js-init.js
+++ GovUk.Frontend.AspNetCore.Extensions/wwwroot/govuk/govuk-js-init.js
@@ -1,5 +1,30 @@
 ﻿import { initAll } from '/govuk/all.min.js';
-initAll();
+const isWelsh = true;
+const config = isWelsh ? {
+    characterCount: {
+        i18n: {
+            charactersAtLimit: 'No characters left (W)',
+            charactersUnderLimit: {
+                other: '%{count} characters to go (W)',
+                one: 'One character to go (W)'
+            },
+            charactersOverLimit: {
+                one: 'One character too many (W)',
+                other: 'You have %{count} characters too many (W)'
+            },
+            wordsUnderLimit: {
+                one: 'You have %{count} word remaining (W)',
+                other: 'You have %{count} words remaining (W)'
+            },
+            wordsAtLimit: 'You have 0 words remaining (W)',
+            wordsOverLimit: {
+                one: 'You have %{count} word too many (W)',
+                other: 'You have %{count} words too many (W)'
+            }
+        }
+    }
+} : {}
+initAll(config);
 Array.prototype.forEach.call(
   document.querySelectorAll(".govuk-button[type=submit]"),
   function (button) {

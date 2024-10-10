e85d97ab2b83d5e7901d628d0208934a5bf28eed
Rick Mason <rick.mason@tpr.gov.uk>
2024-10-10 13:48:50 +0100

    Allow Github Issues references as well as Azure DevOps. Fixes #348.

---
+++ .githooks/Confirm-WorkItemReference.Tests.ps1
@@ -0,0 +1,36 @@
+Describe 'Confirm-WorkItemReference' {
+    BeforeAll {
+        Push-Location $PSScriptRoot
+        $file = "./test-commit.txt"
+        Mock Write-Warning {}
+    }
+
+    It 'Should allow a commit with an Azure DevOps work item reference' {
+        Set-Content -Path $file -Value "Test commit AB#123456"
+        ./Confirm-WorkItemReference.ps1 $file
+        $LASTEXITCODE | Should -Be 0
+    }
+    
+    It 'Should allow a commit with a Github Issues work item reference' {
+        Set-Content -Path $file -Value "Test commit #123"
+        ./Confirm-WorkItemReference $file 
+        $LASTEXITCODE | Should -Be 0
+    }
+
+    It 'Should allow a merge commit without a work item reference' {
+        Set-Content -Path $file -Value "Merge branch 'example' into 'develop'"
+        ./Confirm-WorkItemReference $file 
+        $LASTEXITCODE | Should -Be 0
+    }
+
+    It 'Should block a non-merge commit without a work item reference' {
+        Set-Content -Path $file -Value "Forgotten work item"
+        ./Confirm-WorkItemReference $file 
+        $LASTEXITCODE | Should -Be 1
+    }
+
+    AfterAll {
+        Remove-Item $file
+        Pop-Location
+    }
+}

0e00aea24bd76269f27c3b6e6e07a1c8a89eed8f
Rick Mason <rick.mason@tpr.gov.uk>
2024-12-27 15:54:12 +0000

    Automate adding the horizontal divider below h1 on TPR pages

---
+++ ThePensionsRegulator.Frontend.Umbraco.Tests/Services/TprDividerViewInterceptorTests.cs
@@ -0,0 +1,220 @@
+﻿using GovUk.Frontend.Umbraco;
+using GovUk.Frontend.Umbraco.Blocks;
+using Moq;
+using ThePensionsRegulator.Frontend.Umbraco.Services;
+using ThePensionsRegulator.Umbraco.Blocks;
+using ThePensionsRegulator.Umbraco.Testing;
+using GovUkElementTypeAliases = GovUk.Frontend.Umbraco.ElementTypeAliases;
+using GovUkPropertyAliases = GovUk.Frontend.Umbraco.PropertyAliases;
+
+namespace ThePensionsRegulator.Frontend.Umbraco.Tests.Services
+{
+	public class TprDividerViewInterceptorTests
+	{
+		[TestCase(ElementTypeAliases.TprBox, ElementTypeAliases.TprBoxSettings)]
+		[TestCase(GovUkElementTypeAliases.GridOneQuarter, GovUkElementTypeAliases.GridSingleColumnLayoutSettings)]
+		[TestCase(GovUkElementTypeAliases.GridOneThird, GovUkElementTypeAliases.GridSingleColumnLayoutSettings)]
+		[TestCase(GovUkElementTypeAliases.GridOneHalf, GovUkElementTypeAliases.GridSingleColumnLayoutSettings)]
+		[TestCase(GovUkElementTypeAliases.GridTwoThirds, GovUkElementTypeAliases.GridSingleColumnLayoutSettings)]
+		[TestCase(GovUkElementTypeAliases.GridThreeQuarters, GovUkElementTypeAliases.GridSingleColumnLayoutSettings)]
+		[TestCase(GovUkElementTypeAliases.GridFullWidth, GovUkElementTypeAliases.GridSingleColumnLayoutSettings)]
+		[TestCase(GovUkElementTypeAliases.GridOneQuarterThreeQuarters, GovUkElementTypeAliases.GridTwoColumnLayoutSettings)]
+		[TestCase(GovUkElementTypeAliases.GridOneThirdTwoThirds, GovUkElementTypeAliases.GridTwoColumnLayoutSettings)]
+		[TestCase(GovUkElementTypeAliases.GridTwoEqualColumns, GovUkElementTypeAliases.GridTwoColumnLayoutSettings)]
+		[TestCase(GovUkElementTypeAliases.GridTwoThirdsOneThird, GovUkElementTypeAliases.GridTwoColumnLayoutSettings)]
+		[TestCase(GovUkElementTypeAliases.GridThreeQuartersOneQuarter, GovUkElementTypeAliases.GridTwoColumnLayoutSettings)]
+		[TestCase(GovUkElementTypeAliases.GridThreeEqualColumns, GovUkElementTypeAliases.GridThreeColumnLayoutSettings)]
+		[TestCase(GovUkElementTypeAliases.GridFourEqualColumns, GovUkElementTypeAliases.GridFourColumnLayoutSettings)]
+		public void Layout_block_is_not_updated(string contentAlias, string settingsAlias)
+		{
+			// Arrange
+			var blockViewModel = CreateBlockView(contentAlias, settingsAlias, false);
+
+			var interceptor = new TprDividerViewInterceptor();
+
+			// Act
+			interceptor.InterceptBlockView(blockViewModel);
+
+			// Assert
+			Assert.That(blockViewModel.OpenGridRowAndColumn, Is.False);
+			Assert.That(blockViewModel.CloseGridRowAndColumn, Is.False);
+		}
+
+		[TestCase(true)]
+		[TestCase(false)]
+		public void Grid_row_rendered_and_class_added_to_page_heading_if_missing(bool classWasAlreadyPresent)
+		{
+			// Arrange
+			var blockViewModel = CreateBlockView(GovUkElementTypeAliases.PageHeading, GovUkElementTypeAliases.PageHeadingSettings, classWasAlreadyPresent, classWasAlreadyPresent ? TprClassNames.Divider : null);
+
+			var interceptor = new TprDividerViewInterceptor();
+
+			// Act
+			interceptor.InterceptBlockView(blockViewModel);
+
+			// Assert
+			Assert.That(blockViewModel.RowClasses, Is.EqualTo($"{GovUkClassNames.Row} {TprClassNames.Divider}"));
+			Assert.That(blockViewModel.OpenGridRowAndColumn, Is.True);
+			Assert.That(blockViewModel.CloseGridRowAndColumn, Is.True);
+		}
+
+		[TestCase(GovUkElementTypeAliases.Checkboxes, GovUkElementTypeAliases.CheckboxesSettings, true, true, true)]
+		[TestCase(GovUkElementTypeAliases.Checkboxes, GovUkElementTypeAliases.CheckboxesSettings, true, true, false)]
+		[TestCase(GovUkElementTypeAliases.Checkboxes, GovUkElementTypeAliases.CheckboxesSettings, true, false, false)]
+
+		[TestCase(GovUkElementTypeAliases.DateInput, GovUkElementTypeAliases.DateInputSettings, true, true, true)]
+		[TestCase(GovUkElementTypeAliases.DateInput, GovUkElementTypeAliases.DateInputSettings, true, true, false)]
+		[TestCase(GovUkElementTypeAliases.DateInput, GovUkElementTypeAliases.DateInputSettings, true, false, false)]
+
+		[TestCase(GovUkElementTypeAliases.Fieldset, GovUkElementTypeAliases.FieldsetSettings, true, true, true)]
+		[TestCase(GovUkElementTypeAliases.Fieldset, GovUkElementTypeAliases.FieldsetSettings, true, true, false)]
+		[TestCase(GovUkElementTypeAliases.Fieldset, GovUkElementTypeAliases.FieldsetSettings, true, false, false)]
+
+		[TestCase(GovUkElementTypeAliases.Radios, GovUkElementTypeAliases.RadiosSettings, true, true, true)]
+		[TestCase(GovUkElementTypeAliases.Radios, GovUkElementTypeAliases.RadiosSettings, true, true, false)]
+		[TestCase(GovUkElementTypeAliases.Radios, GovUkElementTypeAliases.RadiosSettings, true, false, false)]
+
+		[TestCase(GovUkElementTypeAliases.FileUpload, GovUkElementTypeAliases.FileUploadSettings, false, true, true)]
+		[TestCase(GovUkElementTypeAliases.FileUpload, GovUkElementTypeAliases.FileUploadSettings, false, true, false)]
+		[TestCase(GovUkElementTypeAliases.FileUpload, GovUkElementTypeAliases.FileUploadSettings, false, false, false)]
+
+		[TestCase(GovUkElementTypeAliases.Select, GovUkElementTypeAliases.SelectSettings, false, true, true)]
+		[TestCase(GovUkElementTypeAliases.Select, GovUkElementTypeAliases.SelectSettings, false, true, false)]
+		[TestCase(GovUkElementTypeAliases.Select, GovUkElementTypeAliases.SelectSettings, false, false, false)]
+
+		[TestCase(GovUkElementTypeAliases.Textarea, GovUkElementTypeAliases.TextareaSettings, false, true, true)]
+		[TestCase(GovUkElementTypeAliases.Textarea, GovUkElementTypeAliases.TextareaSettings, false, true, false)]
+		[TestCase(GovUkElementTypeAliases.Textarea, GovUkElementTypeAliases.TextareaSettings, false, false, false)]
+
+		[TestCase(GovUkElementTypeAliases.TextInput, GovUkElementTypeAliases.TextInputSettings, false, true, true)]
+		[TestCase(GovUkElementTypeAliases.TextInput, GovUkElementTypeAliases.TextInputSettings, false, true, false)]
+		[TestCase(GovUkElementTypeAliases.TextInput, GovUkElementTypeAliases.TextInputSettings, false, false, false)]
+		public void If_component_includes_page_heading_grid_row_rendered_and_class_added_if_missing(string contentAlias, string settingsAlias, bool isFieldset, bool legendIsPageHeading, bool classWasAlreadyPresent)
+		{
+			// Arrange
+			var isPageHeadingProperty = isFieldset ? GovUkPropertyAliases.FieldsetLegendIsPageHeading : GovUkPropertyAliases.LabelIsPageHeading;
+			var dividerClass = isFieldset ? TprClassNames.DividerForFieldsetWithLegendAsPageHeading : TprClassNames.DividerForFormComponentWithLabelAsPageHeading;
+
+			var blockViewModel = CreateBlockView(contentAlias, settingsAlias, classWasAlreadyPresent, classWasAlreadyPresent ? dividerClass : null);
+			Mock.Get(blockViewModel.CurrentBlock.Settings).SetupUmbracoBooleanPropertyValue(isPageHeadingProperty, legendIsPageHeading);
+
+			var interceptor = new TprDividerViewInterceptor();
+
+			// Act
+			interceptor.InterceptBlockView(blockViewModel);
+
+			// Assert
+			if (legendIsPageHeading)
+			{
+				Assert.That(blockViewModel.RowClasses, Is.EqualTo($"{GovUkClassNames.Row} {dividerClass}"));
+				Assert.That(blockViewModel.OpenGridRowAndColumn, Is.True);
+				Assert.That(blockViewModel.CloseGridRowAndColumn, Is.True);
+			}
+			else
+			{
+				Assert.That(blockViewModel.RowClasses, Is.EqualTo(GovUkClassNames.Row));
+				Assert.That(blockViewModel.OpenGridRowAndColumn, Is.False);
+				Assert.That(blockViewModel.CloseGridRowAndColumn, Is.False);
+			}
+		}
+
+		[TestCase(GovUkElementTypeAliases.PageHeading, GovUkElementTypeAliases.PageHeadingSettings, false, false)]
+		[TestCase(GovUkElementTypeAliases.Checkboxes, GovUkElementTypeAliases.CheckboxesSettings, true, false)]
+		[TestCase(GovUkElementTypeAliases.Checkboxes, GovUkElementTypeAliases.CheckboxesSettings, false, false)]
+		[TestCase(GovUkElementTypeAliases.DateInput, GovUkElementTypeAliases.DateInputSettings, true, false)]
+		[TestCase(GovUkElementTypeAliases.DateInput, GovUkElementTypeAliases.DateInputSettings, false, false)]
+		[TestCase(GovUkElementTypeAliases.Fieldset, GovUkElementTypeAliases.FieldsetSettings, true, false)]
+		[TestCase(GovUkElementTypeAliases.Fieldset, GovUkElementTypeAliases.FieldsetSettings, false, false)]
+		[TestCase(GovUkElementTypeAliases.Radios, GovUkElementTypeAliases.RadiosSettings, true, false)]
+		[TestCase(GovUkElementTypeAliases.Radios, GovUkElementTypeAliases.RadiosSettings, false, false)]
+		[TestCase(GovUkElementTypeAliases.FileUpload, GovUkElementTypeAliases.FileUploadSettings, false, true)]
+		[TestCase(GovUkElementTypeAliases.FileUpload, GovUkElementTypeAliases.FileUploadSettings, false, false)]
+		[TestCase(GovUkElementTypeAliases.Select, GovUkElementTypeAliases.SelectSettings, false, true)]
+		[TestCase(GovUkElementTypeAliases.Select, GovUkElementTypeAliases.SelectSettings, false, false)]
+		[TestCase(GovUkElementTypeAliases.Textarea, GovUkElementTypeAliases.TextareaSettings, false, true)]
+		[TestCase(GovUkElementTypeAliases.Textarea, GovUkElementTypeAliases.TextareaSettings, false, false)]
+		[TestCase(GovUkElementTypeAliases.TextInput, GovUkElementTypeAliases.TextInputSettings, false, true)]
+		[TestCase(GovUkElementTypeAliases.TextInput, GovUkElementTypeAliases.TextInputSettings, false, false)]
+		public void Block_following_component_where_class_is_added_sets_OpenGridRowAndColumn_true(string contentAlias, string settingsAlias, bool hasLegendAsHeading, bool hasLabelAsHeading)
+		{
+			// Arrange
+			var isPageHeadingProperty = hasLegendAsHeading ? GovUkPropertyAliases.FieldsetLegendIsPageHeading : hasLabelAsHeading ? GovUkPropertyAliases.LabelIsPageHeading : null;
+
+			var blockViewModel = CreateBlockView("content", "settings", false);
+			blockViewModel.PreviousBlock = CreateBlock(contentAlias, settingsAlias);
+			if (isPageHeadingProperty is not null)
+			{
+				Mock.Get(blockViewModel.PreviousBlock.Settings).SetupUmbracoBooleanPropertyValue(isPageHeadingProperty, true);
+			}
+
+			var interceptor = new TprDividerViewInterceptor();
+
+			// Act
+			interceptor.InterceptBlockView(blockViewModel);
+
+			// Assert
+			Assert.That(blockViewModel.OpenGridRowAndColumn, Is.EqualTo(contentAlias == GovUkElementTypeAliases.PageHeading || hasLegendAsHeading || hasLabelAsHeading));
+		}
+
+		[TestCase(GovUkElementTypeAliases.PageHeading, GovUkElementTypeAliases.PageHeadingSettings, false, false)]
+		[TestCase(GovUkElementTypeAliases.Checkboxes, GovUkElementTypeAliases.CheckboxesSettings, true, false)]
+		[TestCase(GovUkElementTypeAliases.Checkboxes, GovUkElementTypeAliases.CheckboxesSettings, false, false)]
+		[TestCase(GovUkElementTypeAliases.DateInput, GovUkElementTypeAliases.DateInputSettings, true, false)]
+		[TestCase(GovUkElementTypeAliases.DateInput, GovUkElementTypeAliases.DateInputSettings, false, false)]
+		[TestCase(GovUkElementTypeAliases.Fieldset, GovUkElementTypeAliases.FieldsetSettings, true, false)]
+		[TestCase(GovUkElementTypeAliases.Fieldset, GovUkElementTypeAliases.FieldsetSettings, false, false)]
+		[TestCase(GovUkElementTypeAliases.Radios, GovUkElementTypeAliases.RadiosSettings, true, false)]
+		[TestCase(GovUkElementTypeAliases.Radios, GovUkElementTypeAliases.RadiosSettings, false, false)]
+		[TestCase(GovUkElementTypeAliases.FileUpload, GovUkElementTypeAliases.FileUploadSettings, false, true)]
+		[TestCase(GovUkElementTypeAliases.FileUpload, GovUkElementTypeAliases.FileUploadSettings, false, false)]
+		[TestCase(GovUkElementTypeAliases.Select, GovUkElementTypeAliases.SelectSettings, false, true)]
+		[TestCase(GovUkElementTypeAliases.Select, GovUkElementTypeAliases.SelectSettings, false, false)]
+		[TestCase(GovUkElementTypeAliases.Textarea, GovUkElementTypeAliases.TextareaSettings, false, true)]
+		[TestCase(GovUkElementTypeAliases.Textarea, GovUkElementTypeAliases.TextareaSettings, false, false)]
+		[TestCase(GovUkElementTypeAliases.TextInput, GovUkElementTypeAliases.TextInputSettings, false, true)]
+		[TestCase(GovUkElementTypeAliases.TextInput, GovUkElementTypeAliases.TextInputSettings, false, false)]
+		public void Block_preceding_component_where_class_is_added_sets_CloseGridRowAndColumn_true(string contentAlias, string settingsAlias, bool hasLegendAsHeading, bool hasLabelAsHeading)
+		{
+			// Arrange
+			var isPageHeadingProperty = hasLegendAsHeading ? GovUkPropertyAliases.FieldsetLegendIsPageHeading : hasLabelAsHeading ? GovUkPropertyAliases.LabelIsPageHeading : null;
+
+			var blockViewModel = CreateBlockView("content", "settings", false);
+			blockViewModel.NextBlock = CreateBlock(contentAlias, settingsAlias);
+			if (isPageHeadingProperty is not null)
+			{
+				Mock.Get(blockViewModel.NextBlock.Settings).SetupUmbracoBooleanPropertyValue(isPageHeadingProperty, true);
+			}
+
+			var interceptor = new TprDividerViewInterceptor();
+
+			// Act
+			interceptor.InterceptBlockView(blockViewModel);
+
+			// Assert
+			Assert.That(blockViewModel.CloseGridRowAndColumn, Is.EqualTo(contentAlias == GovUkElementTypeAliases.PageHeading || hasLegendAsHeading || hasLabelAsHeading));
+		}
+
+		private static BlockViewModel CreateBlockView(string contentAlias, string settingsAlias, bool renderGridRowInitialValue, string? existingRowClasses = null)
+		{
+			var block = CreateBlock(contentAlias, settingsAlias, existingRowClasses);
+			var blockView = new BlockViewModel
+			{
+				CurrentBlock = block,
+				OpenGridRowAndColumn = renderGridRowInitialValue,
+				CloseGridRowAndColumn = renderGridRowInitialValue
+			};
+			if (existingRowClasses is not null) { blockView.RowClasses += $" {existingRowClasses}"; }
+			return blockView;
+		}
+
+		private static OverridableBlockGridItem CreateBlock(string contentAlias, string settingsAlias, string? existingRowClasses = null)
+		{
+			var settings = UmbracoBlockGridFactory.CreateContentOrSettings(settingsAlias);
+			if (existingRowClasses is not null) { settings.SetupUmbracoTextboxPropertyValue(GovUkPropertyAliases.CssClassesForRow, existingRowClasses); }
+			return UmbracoBlockGridFactory.CreateOverridableBlock(
+						UmbracoBlockGridFactory.CreateContentOrSettings(contentAlias).Object,
+						settings.Object
+					);
+		}
+	}
+}
---
+++ ThePensionsRegulator.Frontend.Umbraco/Services/TprDividerViewInterceptor.cs
@@ -0,0 +1,72 @@
+﻿using GovUk.Frontend.Umbraco.Blocks;
+using System;
+using System.Collections.Generic;
+using ThePensionsRegulator.Umbraco;
+using ThePensionsRegulator.Umbraco.Blocks;
+using GovUkElementTypeAliases = GovUk.Frontend.Umbraco.ElementTypeAliases;
+using GovUkPropertyAliases = GovUk.Frontend.Umbraco.PropertyAliases;
+
+namespace ThePensionsRegulator.Frontend.Umbraco.Services
+{
+	/// <summary>
+	/// Adds a decorative horizontal divider after the main heading on a page.
+	/// </summary>
+	public class TprDividerViewInterceptor : IBlockViewInterceptor
+	{
+		private List<string> _fieldsetAliases = [GovUkElementTypeAliases.Checkboxes, GovUkElementTypeAliases.DateInput, GovUkElementTypeAliases.Fieldset, GovUkElementTypeAliases.Radios];
+		private List<string> _labelAliases = [GovUkElementTypeAliases.FileUpload, GovUkElementTypeAliases.Select, GovUkElementTypeAliases.Textarea, GovUkElementTypeAliases.TextInput];
+
+		/// <inheritdoc/>
+		public void InterceptBlockView(BlockViewModel blockViewModel)
+		{
+			if (IsLayoutBlock(blockViewModel.CurrentBlock)) { return; }
+
+			var blockAlias = blockViewModel.CurrentBlock.Content.ContentType.Alias;
+			if (blockAlias == GovUkElementTypeAliases.PageHeading)
+			{
+				ApplyClassToRowAndForceRowToRender(blockViewModel, TprClassNames.Divider);
+			}
+
+			if (_fieldsetAliases.Contains(blockAlias) && blockViewModel.CurrentBlock!.Settings.Value<bool>(GovUkPropertyAliases.FieldsetLegendIsPageHeading))
+			{
+				ApplyClassToRowAndForceRowToRender(blockViewModel, TprClassNames.DividerForFieldsetWithLegendAsPageHeading);
+			}
+
+			if (_labelAliases.Contains(blockAlias) && blockViewModel.CurrentBlock!.Settings.Value<bool>(GovUkPropertyAliases.LabelIsPageHeading))
+			{
+				ApplyClassToRowAndForceRowToRender(blockViewModel, TprClassNames.DividerForFormComponentWithLabelAsPageHeading);
+			}
+
+			if (AdjacentBlockRequiresClass(blockViewModel.PreviousBlock)) { blockViewModel.OpenGridRowAndColumn = true; }
+			if (AdjacentBlockRequiresClass(blockViewModel.NextBlock)) { blockViewModel.CloseGridRowAndColumn = true; }
+		}
+
+		private bool AdjacentBlockRequiresClass(IOverridableBlockReference<IOverridablePublishedElement, IOverridablePublishedElement>? adjacentBlock)
+		{
+			var aliasOfAdjacentBlock = adjacentBlock?.Content.ContentType.Alias ?? string.Empty;
+			return (aliasOfAdjacentBlock == GovUkElementTypeAliases.PageHeading
+				|| (_fieldsetAliases.Contains(aliasOfAdjacentBlock) && adjacentBlock!.Settings.Value<bool>(GovUkPropertyAliases.FieldsetLegendIsPageHeading))
+				|| (_labelAliases.Contains(aliasOfAdjacentBlock) && adjacentBlock!.Settings.Value<bool>(GovUkPropertyAliases.LabelIsPageHeading))
+				);
+		}
+
+		/// <summary>
+		/// Layout blocks are those which take control of rendering width containers, grid rows and columns in a block grid.
+		/// </summary>
+		private static bool IsLayoutBlock(IOverridableBlockReference<IOverridablePublishedElement, IOverridablePublishedElement> block)
+		{
+			return block.Content.ContentType.Alias == ElementTypeAliases.TprBox || block.Content.ContentType.Alias.StartsWith("govukGrid", StringComparison.OrdinalIgnoreCase);
+		}
+
+		private static void ApplyClassToRowAndForceRowToRender(BlockViewModel blockViewModel, string className)
+		{
+			var classList = blockViewModel.CurrentBlock.GridRowClassList();
+			if (!classList.Contains(className))
+			{
+				blockViewModel.RowClasses += $" {className}";
+				blockViewModel.OpenGridRowAndColumn = true;
+				blockViewModel.CloseGridRowAndColumn = true;
+			}
+		}
+	}
+}

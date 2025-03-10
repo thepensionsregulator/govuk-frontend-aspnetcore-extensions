a866a9615c5b0a4ea5bee7fac01ccfc6e921b4f5
ben walker <ben.walker@tpr.gov.uk>
2025-03-10 09:45:56 +0000

    #AB218010 Initial PoC

---
+++ ThePensionsRegulator.Frontend/TagHelpers/TprTimelineItemTagHelper.cs
@@ -0,0 +1,60 @@
+﻿using GovUk.Frontend.AspNetCore.Extensions;
+using GovUk.Frontend.AspNetCore;
+using Microsoft.AspNetCore.Razor.TagHelpers;
+using System.Threading.Tasks;
+using GovUk.Frontend.AspNetCore.Extensions.HtmlGeneration;
+using System.Collections.Generic;
+using ComponentGenerator = ThePensionsRegulator.Frontend.HtmlGeneration.ComponentGenerator;
+using System;
+using ThePensionsRegulator.Frontend.HtmlGeneration;
+using Microsoft.AspNetCore.Mvc.ViewFeatures;
+using System.Diagnostics;
+
+namespace ThePensionsRegulator.Frontend.TagHelpers
+{
+    [HtmlTargetElement(TagName)]
+    [OutputElementHint(ComponentGenerator.TimelineItemElement)]
+    public class TprTimelineItemTagHelper : TagHelper
+    {
+        internal const string TagName = "tpr-timeline-item";
+
+        /// <summary>
+        /// Additional attributes to add to the generated <c>a</c> element where <c>.govuk-link</c> is applied.
+        /// </summary>
+        //[HtmlAttributeName(DictionaryAttributePrefix = LinkAttributesPrefix)]
+
+        //public IDictionary<string, string?>? LinkAttributes { get; set; } = new Dictionary<string, string?>();
+
+        /// <summary>
+        /// A link to the task.
+        /// </summary>
+        [HtmlAttributeName("date")]
+        public string? Date { get; set; } = "";
+
+        [HtmlAttributeName("heading")]
+        public string? Heading { get; set; } = "";
+
+        /// <inheritdoc/>
+        public override async Task ProcessAsync(TagHelperContext context, TagHelperOutput output)
+        {
+            var taskListContext = context.GetContextItem<TprTimelineContext>();
+
+            var itemContext = new TprTimelineItemContext();
+
+            using (context.SetScopedContextItem(itemContext))
+            {
+                await output.GetChildContentAsync();
+            }            
+
+            taskListContext.AddItem(new TprTimelineItem
+            {
+                Attributes = output.Attributes.ToAttributeDictionary(),
+                Heading = Heading,
+                DateTime = Date,
+                Content = itemContext.HtmlContent
+            });
+
+            output.SuppressOutput();
+        }
+    }
+}
---
+++ ThePensionsRegulator.Frontend/TagHelpers/TprTimelineTagHelper.cs
@@ -0,0 +1,59 @@
+﻿using GovUk.Frontend.AspNetCore.Extensions;
+using GovUk.Frontend.AspNetCore;
+using Microsoft.AspNetCore.Html;
+using Microsoft.AspNetCore.Mvc.TagHelpers;
+using Microsoft.AspNetCore.Razor.TagHelpers;
+using System.Threading.Tasks;
+using ThePensionsRegulator.Frontend.HtmlGeneration;
+//using ComponentGenerator = ThePensionsRegulator.Frontend.HtmlGeneration.ComponentGenerator;
+
+namespace ThePensionsRegulator.Frontend.TagHelpers
+{
+    [HtmlTargetElement(TagName)]
+    [RestrictChildren(TprTimelineItemTagHelper.TagName)]
+    [OutputElementHint(ComponentGenerator.TimelineElement)]
+    public class TprTimelineTagHelper : TagHelper
+    {
+        internal const string TagName = "tpr-timeline";
+
+        private readonly ITprHtmlGenerator _htmlGenerator;
+
+        /// <summary>
+        /// Creates a new <see cref="TprFooterBarTagHelper"/>.
+        /// </summary>
+        public TprTimelineTagHelper()
+            : this(htmlGenerator: null)
+        {
+        }
+
+        internal TprTimelineTagHelper(ITprHtmlGenerator? htmlGenerator)
+        {
+            _htmlGenerator = htmlGenerator ?? new ComponentGenerator();
+        }
+
+        /// <inheritdoc/>
+        public override async Task ProcessAsync(TagHelperContext context, TagHelperOutput output)
+        {
+            var timelineContext = new TprTimelineContext();
+
+            using (context.SetScopedContextItem(timelineContext))
+            {
+                await output.GetChildContentAsync();
+            }
+
+            timelineContext.ThrowIfIncomplete();
+
+            var tagBuilder = _htmlGenerator.GenerateTprTimeline(
+                output.Attributes.ToAttributeDictionary(),
+                timelineContext.Tasks
+            );
+
+            output.TagName = tagBuilder.TagName;
+            output.TagMode = TagMode.StartTagAndEndTag;
+
+            output.Attributes.Clear();
+            output.MergeAttributes(tagBuilder);
+            output.Content.SetHtmlContent(tagBuilder.InnerHtml);
+        }
+    }
+}

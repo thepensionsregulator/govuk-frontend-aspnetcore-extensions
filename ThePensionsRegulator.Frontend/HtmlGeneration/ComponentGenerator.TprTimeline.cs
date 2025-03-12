1aa6073c5fffccb27165e202caca68f85698c8fb
ZebSadiq <zeb.sadiq@tpr.gov.uk>
2025-03-12 10:39:24 +0000

    Timeline component added thanks to Ben Walker's POC

---
+++ ThePensionsRegulator.Frontend/HtmlGeneration/ComponentGenerator.TprTimeline.cs
@@ -0,0 +1,72 @@
+using GovUk.Frontend.AspNetCore;
+using GovUk.Frontend.AspNetCore.Extensions;
+using Microsoft.AspNetCore.Authentication;
+using Microsoft.AspNetCore.Mvc.Rendering;
+using Microsoft.AspNetCore.Mvc.ViewFeatures;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace ThePensionsRegulator.Frontend.HtmlGeneration
+{
+    public partial class ComponentGenerator
+    {
+        internal const string TimelineElement = "div";
+        internal const string TimelineItemElement = "li";
+
+        public virtual TagBuilder GenerateTprTimeline(
+            AttributeDictionary? attributes,
+            IEnumerable<TprTimelineItem> items)
+        {
+            Guard.ArgumentNotNull(nameof(items), items);
+            Guard.ArgumentValid(nameof(items), "A Timeline must contain at least one item", items.Any());
+
+            var timelineTagBuilder = new TagBuilder(TimelineElement);
+            if (attributes is not null) { timelineTagBuilder.MergeAttributes(attributes); }
+            timelineTagBuilder.MergeCssClass("tpr-timeline");
+
+            var innerItems = new TagBuilder("ol");
+            innerItems.MergeCssClass("tpr-timeline__items");
+
+            foreach (var item in items)
+            {
+                Guard.ArgumentValid(nameof(item), "Date cannot be null or empty", item.DateTime != null);
+                Guard.ArgumentValid(nameof(item), "Heading cannot be null or empty", item.Heading != null);
+
+                var itemBuilder = new TagBuilder(TimelineItemElement);
+                itemBuilder.MergeCssClass("tpr-timeline__item");
+
+                if (item.Attributes is not null) { itemBuilder.MergeAttributes(item.Attributes); }
+                innerItems.InnerHtml.AppendHtml(itemBuilder);
+
+                itemBuilder.InnerHtml.AppendHtml(BuildDateTime(item));
+                itemBuilder.InnerHtml.AppendHtml(BuildHeading(item));
+
+                if (item.Content != null)
+                {
+                    itemBuilder.InnerHtml.AppendHtml(item.Content);
+                }
+            }
+
+            timelineTagBuilder.InnerHtml.AppendHtml(innerItems);
+
+            return timelineTagBuilder;
+        }
+
+        private static TagBuilder BuildDateTime(TprTimelineItem item)
+        {
+            var timelineItemDateTime = new TagBuilder("p");
+            timelineItemDateTime.MergeCssClass("tpr-timeline__datetime");
+            timelineItemDateTime.InnerHtml.AppendHtml(item.DateTime!.ToString());
+            return timelineItemDateTime;
+        }
+
+        private static TagBuilder BuildHeading(TprTimelineItem item) {
+            var timelineItemHeading = new TagBuilder("h2");
+            timelineItemHeading.MergeCssClass("tpr-timeline__heading");
+            timelineItemHeading.InnerHtml.AppendHtml(item.Heading!.ToString());
+            return timelineItemHeading;
+        }
+
+    }
+}
---
+++ ThePensionsRegulator.Frontend/TagHelpers/TprTimelineContext.cs
@@ -0,0 +1,40 @@
+﻿using GovUk.Frontend.AspNetCore.Extensions.TagHelpers;
+using GovUk.Frontend.AspNetCore.Extensions;
+using GovUk.Frontend.AspNetCore;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using ThePensionsRegulator.Frontend.HtmlGeneration;
+
+namespace ThePensionsRegulator.Frontend.TagHelpers
+{
+    public class TprTimelineContext
+    {
+        private readonly List<TprTimelineItem> _items;
+
+        public TprTimelineContext()
+        {
+            _items = new List<TprTimelineItem>();
+        }
+
+        public IReadOnlyList<TprTimelineItem> Tasks => _items;
+
+        public void AddItem(TprTimelineItem item)
+        {
+            Guard.ArgumentNotNull(nameof(item), item);
+
+            _items.Add(item);
+        }
+
+        public void ThrowIfIncomplete()
+        {
+            if (Tasks.Count < 1)
+            {
+                throw ExceptionHelper.AChildElementMustBeProvided(TprTimelineItemTagHelper.TagName);
+            }
+        }
+
+    }
+}

7561bfc31d1cfe99b152a4edbecfc32d3e34d765
Rick Mason <rick.mason@tpr.gov.uk>
2025-04-11 15:14:11 +0100

    Upgrade to govuk-frontend-aspnetcore v2.5.0 and GOV.UK Frontend v5.5.0 #370

---
+++ GovUk.Frontend.AspNetCore.Extensions.ConformanceTests/TaskListConverter.cs
@@ -0,0 +1,51 @@
+﻿using GovUk.Frontend.AspNetCore.Extensions.ConformanceTests.OptionsJson;
+using Newtonsoft.Json;
+using Newtonsoft.Json.Converters;
+using Newtonsoft.Json.Linq;
+
+namespace GovUk.Frontend.AspNetCore.Extensions.ConformanceTests
+{
+    /// <summary>
+    /// A custom converter is required because the <c>items</c> collection can contain an object, boolean <c>false</c> or empty string, and .NET needs help to deserialise that.
+    /// </summary>
+    internal class TaskListConverter : CustomCreationConverter<TaskList>
+    {
+        public override TaskList Create(Type objectType)
+        {
+            throw new NotImplementedException();
+        }
+
+        public TaskList Create(JObject jObject)
+        {
+            var taskList = new TaskList();
+
+            var items = jObject.Property("items")?.ToArray()?.First();
+            if (items is not null)
+            {
+                foreach (var item in items)
+                {
+                    var task = (item.Type != JTokenType.Boolean) ? JsonConvert.DeserializeObject<TaskListTask>(item.ToString()) : null;
+                    taskList.Items.Add(task);
+                }
+            }
+
+            return taskList;
+        }
+
+        public override object ReadJson(JsonReader reader, Type objectType, object? existingValue, JsonSerializer serializer)
+        {
+            // Load JObject from stream 
+            JObject jObject = JObject.Load(reader);
+
+            // Create target object based on JObject 
+            var target = Create(jObject);
+
+            // Populate the object properties 
+            jObject.Remove("items");
+            serializer.Populate(jObject.CreateReader(), target);
+
+            return target;
+        }
+    }
+
+}

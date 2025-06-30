ef73575bee5a8361a44f89b27f5ab0ed430be766
Ethan Lambourne <Ethan.Lambourne@tpr.gov.uk>
2025-06-30 16:03:11 +0100

    Removing previous implementations of the breadcrumb and replacing it with the new one

---
+++ GovUk.Frontend.Umbraco.ExampleApp/Components/RenderBreadcrumbComponent.cs
@@ -0,0 +1,34 @@
+﻿using Microsoft.AspNetCore.Mvc;
+using ThePensionsRegulator.Frontend.Umbraco.Models;
+using ThePensionsRegulator.Frontend.Umbraco.Services;
+using ThePensionsRegulator.Umbraco;
+
+namespace GovUk.Frontend.Umbraco.ExampleApp.Components
+{
+    [ViewComponent(Name = "Breadcrumb")]
+    public class RenderBreadcrumbComponent : ViewComponent
+    {
+        private BreadcrumbViewModel ViewModel;
+
+        public RenderBreadcrumbComponent(IBreadcrumbLinksService breadcrumbLinksService, IUmbracoPublishedContentAccessor publishedContext)
+        {
+            if (breadcrumbLinksService is not null && publishedContext is not null)
+            {
+                ViewModel = breadcrumbLinksService.GetLinks(publishedContext.PublishedContent);
+            }
+            else if (breadcrumbLinksService is null)
+            {
+                ViewModel.Error = "Breadcrumb links service is not initialised.";
+            }
+            else
+            {
+                ViewModel.Error = "Published context is not initialised.";
+            }
+        }
+
+        public IViewComponentResult Invoke()
+        {
+            return View("Breadcrumb", ViewModel);
+        }
+    }
+}

801a81b1a40e9d7769d0e8b61b60c08fd6720ed0
Ethan Lambourne <100215369+Ethan-Lambourne@users.noreply.github.com>
2025-07-09 11:51:39 +0100

    #229347 Amend breadcrumb component to use different fields (#454)
    * Basic working implementation
    
    * Removing previous implementations of the breadcrumb and replacing it with the new one
    
    * Create breadcrumb.md
    
    * Changed error handling for implementation on ExampleApp
    
    ---------
    
    Co-authored-by: Ethan Lambourne <Ethan.Lambourne@tpr.gov.uk>
    Co-authored-by: villarin <otto@tbtech.co.uk>

---
+++ GovUk.Frontend.Umbraco.ExampleApp/Components/RenderBreadcrumbComponent.cs
@@ -0,0 +1,37 @@
+﻿using Microsoft.AspNetCore.Mvc;
+using System;
+using ThePensionsRegulator.Frontend.Umbraco.Models;
+using ThePensionsRegulator.Frontend.Umbraco.Services;
+using ThePensionsRegulator.Umbraco;
+
+namespace GovUk.Frontend.Umbraco.ExampleApp.Components
+{
+    [ViewComponent(Name = "Breadcrumb")]
+    public class RenderBreadcrumbComponent : ViewComponent
+    {
+        private BreadcrumbViewModel ViewModel;
+
+        public RenderBreadcrumbComponent(IBreadcrumbLinksService breadcrumbLinksService, IUmbracoPublishedContentAccessor publishedContext)
+        {
+            if (breadcrumbLinksService is not null && publishedContext is not null)
+            {
+                ViewModel = breadcrumbLinksService.GetLinks(publishedContext.PublishedContent);
+            }
+            else if (breadcrumbLinksService is null)
+            {
+                ViewModel.Error = "Breadcrumb links service is not initialised.";
+                throw new ArgumentNullException(nameof(breadcrumbLinksService), ViewModel.Error);
+            }
+            else
+            {
+                ViewModel.Error = "Published context is not initialised.";
+                throw new ArgumentNullException(nameof(publishedContext), ViewModel.Error);
+            }
+        }
+
+        public IViewComponentResult Invoke()
+        {
+            return View("Breadcrumb", ViewModel);
+        }
+    }
+}
---
+++ GovUk.Frontend.Umbraco.ExampleApp/Services/BreadcrumbLinksServiceForExampleApp.cs
@@ -0,0 +1,22 @@
+﻿using System.Linq;
+using ThePensionsRegulator.Frontend.Umbraco.Models;
+using ThePensionsRegulator.Frontend.Umbraco.Services;
+using Umbraco.Cms.Core.Models.PublishedContent;
+using Umbraco.Extensions;
+
+namespace GovUk.Frontend.Umbraco.ExampleApp.Services
+{
+    public class BreadcrumbLinksServiceForExampleApp : IBreadcrumbLinksService
+    {
+        public BreadcrumbViewModel GetLinks(IPublishedContent Page)
+        {
+            BreadcrumbViewModel breadcrumbViewModel = new();
+            foreach (var ancestor in Page.Ancestors().OrderBy(x => x.Level)) 
+            {
+                breadcrumbViewModel.Ancestors.Add(new BreadcrumbLink { Name = ancestor.Name, Url = ancestor.Url() }); 
+            }
+            breadcrumbViewModel.CurrentPage = Page;
+            return breadcrumbViewModel;
+        }
+    }
+}

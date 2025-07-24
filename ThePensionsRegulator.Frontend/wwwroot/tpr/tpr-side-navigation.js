b41352911e43e1e38ff18d44a3c87d3274328f59
Ethan Lambourne <Ethan.Lambourne@tpr.gov.uk>
2025-07-24 16:25:04 +0100

    added mobile functionality with and without javascript; updated example page wording; made a backoffice view; updated css classes.

---
+++ ThePensionsRegulator.Frontend/wwwroot/tpr/tpr-side-navigation.js
@@ -0,0 +1,17 @@
+﻿
+document.addEventListener("DOMContentLoaded", function () {
+    var toggle = document.getElementsByClassName("side-nav-mobile-expand-toggle");
+    for (var i = 0; i < toggle.length; i++) {
+        toggle[i].addEventListener("click", ExpandOrRetractSideNav);
+    }
+});
+
+function ExpandOrRetractSideNav() {
+    var content = this.parentNode.nextElementSibling;
+    if (content.style.maxHeight !== "0px") {
+        content.style.maxHeight = "0px";
+    }
+    else {
+        content.style.maxHeight = content.scrollHeight + "px";
+    }
+}

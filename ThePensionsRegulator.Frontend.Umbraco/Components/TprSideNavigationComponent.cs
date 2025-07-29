c215dfc34deb14d3a95409f1211a17768eda19c0
Ethan Lambourne <Ethan.Lambourne@tpr.gov.uk>
2025-07-29 17:45:39 +0100

    Added View Component implementation

---
+++ ThePensionsRegulator.Frontend.Umbraco/Components/TprSideNavigationComponent.cs
@@ -0,0 +1,17 @@
+﻿using Microsoft.AspNetCore.Mvc;
+using ThePensionsRegulator.Frontend.Umbraco.Models;
+using Umbraco.Cms.Core.Models;
+using Umbraco.Cms.Core.Models.PublishedContent;
+
+namespace ThePensionsRegulator.Frontend.Umbraco.Components
+{
+    [ViewComponent(Name = "TprSideNavigation")]
+    public class TprSideNavigationComponent : ViewComponent
+    {
+        public IViewComponentResult Invoke(IPublishedContent currentPage, IPublishedContent rootAncestor, string[] blacklistedUrls)
+        {
+            TprSideNavigationViewModel ViewModel = new() { CurrentPage = currentPage, RootAncestor = rootAncestor, BlacklistedUrls = blacklistedUrls };
+            return View("SideNavigation", ViewModel);
+        }
+    }
+}
---
+++ ThePensionsRegulator.Frontend.Umbraco/Models/TprSideNavigationViewModel.cs
@@ -0,0 +1,11 @@
+﻿using Umbraco.Cms.Core.Models.PublishedContent;
+
+namespace ThePensionsRegulator.Frontend.Umbraco.Models
+{
+    public class TprSideNavigationViewModel
+    {
+        public IPublishedContent CurrentPage { get; set; }
+        public IPublishedContent RootAncestor { get; set; }
+        public string[] BlacklistedUrls { get; set; } = [];
+    }
+}

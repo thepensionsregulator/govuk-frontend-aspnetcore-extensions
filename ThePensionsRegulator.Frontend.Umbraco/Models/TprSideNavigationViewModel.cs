663d1b6fa93e9028740d5b7da474199d8329f4ef
Ethan Lambourne <Ethan.Lambourne@tpr.gov.uk>
2025-08-19 11:50:14 +0100

    Logic for example app service implementation

--- ThePensionsRegulator.Frontend.Umbraco/Models/TprSideNavigationViewModel.cs
+++ ThePensionsRegulator.Frontend.Umbraco/Models/TprSideNavigationViewModel.cs
@@ -5,14 +5,14 @@ namespace ThePensionsRegulator.Frontend.Umbraco.Models
     public class TprSideNavigationViewModel
     {
         public TprSideNavigationLink TitleLink { get; set; }
-        public List<TprSideNavigationLink> NavigationLinks { get; set; }
+        public List<TprSideNavigationLink> NavigationLinks { get; set; } = new();
     }
 
     public class TprSideNavigationLink
     {
         public string Name { get; set; }
         public string Url { get; set; }
-        bool IsCurrentPage { get; set; }
-        List<TprSideNavigationLink> Children { get; set; }
+        public bool IsCurrentPage { get; set; }
+        public List<TprSideNavigationLink> Children { get; set; } = new();
     }
 }

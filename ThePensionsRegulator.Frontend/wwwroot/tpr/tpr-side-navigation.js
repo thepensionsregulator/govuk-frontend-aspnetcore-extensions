b00e5bebc0b6ac1da9fb0435c61988684d6610c6
Ethan Lambourne <Ethan.Lambourne@tpr.gov.uk>
2025-08-20 11:54:05 +0100

    reverting mobile menu fix

--- ThePensionsRegulator.Frontend/wwwroot/tpr/tpr-side-navigation.js
+++ ThePensionsRegulator.Frontend/wwwroot/tpr/tpr-side-navigation.js
@@ -8,12 +8,12 @@ document.addEventListener("DOMContentLoaded", function () {
 
 function ExpandOrRetractSideNav() {
     var content = this.parentNode.nextElementSibling;
-    if (content.style.maxHeight !== "0px" && content.style.display !== "none") {
+    if (content.style.maxHeight !== "0px") {
         content.style.maxHeight = "0px";
-        content.style.display = "none";
+        //content.style.display = "none";
     }
     else {
-        content.style.display = "block";
+        //content.style.display = "block";
         content.style.maxHeight = content.scrollHeight + "px";
     }
 }

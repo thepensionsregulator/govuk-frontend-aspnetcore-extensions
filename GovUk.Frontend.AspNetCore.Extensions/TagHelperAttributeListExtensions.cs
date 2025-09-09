338aa073fdc1f4775c8ee90325df208820bb15cf
Samuel Hägglund <107886283+samuelhaa@users.noreply.github.com>
2025-09-09 11:31:04 +0100

    Copy deleted file TagHelperAttributeListExtensions.cs from Gunndabad
    Because
    - It was removed in the Gunndabad project and the simplest tolution was
      to copy it here. We may need to re-think whether we need it or
      refactor it to use its replacement, AttributeCollection

---
+++ GovUk.Frontend.AspNetCore.Extensions/TagHelperAttributeListExtensions.cs
@@ -0,0 +1,37 @@
+﻿using Microsoft.AspNetCore.Html;
+using Microsoft.AspNetCore.Mvc.ViewFeatures;
+using Microsoft.AspNetCore.Razor.TagHelpers;
+using System.Web;
+
+namespace GovUk.Frontend.AspNetCore.Extensions
+{
+    /// <summary>
+    /// Utility extensions for <see cref="TagHelperAttributeList"/>.
+    /// </summary>
+    internal static class TagHelperAttributeListExtensions
+    {
+        /// <summary>
+        /// Creates an <see cref="AttributeDictionary"/> from a <see cref="TagHelperAttributeList"/>.
+        /// </summary>
+        /// <param name="list">The <see cref="TagHelperAttributeList"/> to retrieve attributes from.</param>
+        public static AttributeDictionary ToAttributeDictionary(this TagHelperAttributeList? list)
+        {
+            var attributeDictionary = new AttributeDictionary();
+
+            if (list is not null)
+            {
+                foreach (var attribute in list)
+                {
+                    attributeDictionary.Add(
+                        attribute.Name,
+                        attribute.ValueStyle == HtmlAttributeValueStyle.Minimized ?
+                            string.Empty :
+                            attribute.Value is HtmlString htmlString ? HttpUtility.HtmlDecode(htmlString.Value) :
+                            (attribute.Value ?? string.Empty).ToString());
+                }
+            }
+
+            return attributeDictionary;
+        }
+    }
+}

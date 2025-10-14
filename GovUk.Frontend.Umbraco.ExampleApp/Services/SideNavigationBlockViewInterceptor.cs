a70915f36f9bef4498a1c61c262904263ffe6011
Rick Mason <rick.mason@tpr.gov.uk>
2025-10-14 11:38:32 +0100

    The side navigation example page has wanted extra spacing AB#243450

---
+++ GovUk.Frontend.Umbraco.ExampleApp/Services/SideNavigationBlockViewInterceptor.cs
@@ -0,0 +1,17 @@
+﻿using GovUk.Frontend.Umbraco.Blocks;
+using ThePensionsRegulator.Umbraco;
+
+namespace GovUk.Frontend.Umbraco.ExampleApp.Services
+{
+    public class SideNavigationBlockViewInterceptor(IUmbracoPublishedContentAccessor _publishedContentAccessor) : IBlockViewInterceptor
+    {
+        public void InterceptBlockView(BlockViewModel blockViewModel)
+        {
+            if (_publishedContentAccessor.PublishedContent.ContentType.Alias == "sideNavigation")
+            {
+                blockViewModel.OpenWidthContainer = false;
+                blockViewModel.CloseWidthContainer = false;
+            }
+        }
+    }
+}

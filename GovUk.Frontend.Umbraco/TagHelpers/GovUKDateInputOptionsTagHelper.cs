0f99f6ce0033b2d3ad21a47ef36f096bd6e81c5a
Harry Ammon <harry.ammon@tpr.gov.uk>
2025-12-17 11:29:38 +0000

    Create a tag helper to show the year, and hide the year if this is false AB#250195

--- GovUk.Frontend.Umbraco/TagHelpers/GovUKDateInputOptionsTagHelper.cs
+++ GovUk.Frontend.Umbraco/TagHelpers/GovUKDateInputOptionsTagHelper.cs
@@ -1,7 +1,5 @@
 ﻿using GovUk.Frontend.Umbraco.HtmlGeneration;
 using Microsoft.AspNetCore.Razor.TagHelpers;
-using System;
-using System.Threading.Tasks;
 
 namespace GovUk.Frontend.Umbraco.TagHelpers
 {
@@ -22,13 +20,24 @@ namespace GovUk.Frontend.Umbraco.TagHelpers
         [HtmlAttributeName("show-day")]
         public bool ShowDay { get; set; } = true;
 
+        /// <summary>
+        /// Gets or sets whether to the show the year field.
+        /// </summary>
+        [HtmlAttributeName("show-year")]
+        public bool ShowYear { get; set; } = true;
+
         public override async Task ProcessAsync(TagHelperContext context, TagHelperOutput output)
         {
+            if (!ShowDay && !ShowYear)
+            {
+                throw new InvalidOperationException("At least one of 'show-day' or 'show-year' must be true.");
+            }
+
             // Grab the HTML that would've been rendered by the child tag helper.
             var html = (await output.GetChildContentAsync()).GetContent();
             output.SuppressOutput();
 
-            html = _htmlEnhancer.EnhanceHtml(html, ShowDay);
+            html = _htmlEnhancer.EnhanceHtml(html, ShowDay, ShowYear);
 
             // Output the child HTML with any modifications made
             output.Content.AppendHtml(html);

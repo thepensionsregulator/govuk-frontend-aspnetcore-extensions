fc4cf5ff1c5a601c09da083bf28f43b09c2c720a
Rick Mason <rick.mason@tpr.gov.uk>
2025-12-19 10:46:37 +0000

    Add caching to fonts and images #535

---
+++ GovUk.Frontend.AspNetCore.Extensions/Caching/CachingConstants.cs
@@ -0,0 +1,7 @@
+﻿namespace GovUk.Frontend.AspNetCore.Extensions.Caching
+{
+    public static class CachingConstants
+    {
+        public const string StaticAssetVersionQueryParamName = "v";
+    }
+}

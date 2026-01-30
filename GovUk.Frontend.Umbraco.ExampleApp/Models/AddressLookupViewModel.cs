f592fac9adc30d809071f17020258344e172f822
Harry Ammon <harry.ammon@tpr.gov.uk>
2026-01-30 09:51:21 +0000

    Add example of 2 components on one page, Add example of setting value of a field AB#250570

--- GovUk.Frontend.Umbraco.ExampleApp/Models/AddressLookupViewModel.cs
+++ GovUk.Frontend.Umbraco.ExampleApp/Models/AddressLookupViewModel.cs
@@ -8,24 +8,44 @@ namespace GovUk.Frontend.Umbraco.ExampleApp.Models
     {
         public AddressLookup? Page { get; set; }
 
-        [Required(ErrorMessage = nameof(AddressLine1))]
-        [MaxLength(500, ErrorMessage = nameof(AddressLine1))]
-        public string? AddressLine1 { get; set; }
+        [Required(ErrorMessage = nameof(ShippingAddressLine1))]
+        [MaxLength(500, ErrorMessage = nameof(ShippingAddressLine1))]
+        public string? ShippingAddressLine1 { get; set; }
 
-        [MaxLength(500, ErrorMessage = nameof(AddressLine2))]
-        public string? AddressLine2 { get; set; }
+        [MaxLength(500, ErrorMessage = nameof(ShippingAddressLine2))]
+        public string? ShippingAddressLine2 { get; set; }
 
-        [Required(ErrorMessage = nameof(Town))]
-        [MaxLength(500, ErrorMessage = nameof(Town))]
-        public string? Town { get; set; }
+        [Required(ErrorMessage = nameof(ShippingSomethingReallyRandom))]
+        [MaxLength(500, ErrorMessage = nameof(ShippingSomethingReallyRandom))]
+        public string? ShippingSomethingReallyRandom { get; set; }
 
-        [MaxLength(500, ErrorMessage = nameof(Town))]
-        public string? County { get; set; }
+        [MaxLength(500, ErrorMessage = nameof(ShippingCounty))]
+        public string? ShippingCounty { get; set; }
 
-        [Required(ErrorMessage = nameof(Country))]
-        public string? Country { get; set; }
+        [Required(ErrorMessage = nameof(ShippingCountry))]
+        public string? ShippingCountry { get; set; }
 
-        [MaxLength(20, ErrorMessage = nameof(Postcode))]
-        public string? Postcode { get; set; }
+        [MaxLength(20, ErrorMessage = nameof(ShippingPostcode))]
+        public string? ShippingPostcode { get; set; }
+
+        [Required(ErrorMessage = nameof(BillingAddressLine1))]
+        [MaxLength(500, ErrorMessage = nameof(BillingAddressLine1))]
+        public string? BillingAddressLine1 { get; set; }
+
+        [MaxLength(500, ErrorMessage = nameof(BillingAddressLine2))]
+        public string? BillingAddressLine2 { get; set; }
+
+        [Required(ErrorMessage = nameof(BillingSomethingReallyRandom))]
+        [MaxLength(500, ErrorMessage = nameof(BillingSomethingReallyRandom))]
+        public string? BillingSomethingReallyRandom { get; set; }
+
+        [MaxLength(500, ErrorMessage = nameof(BillingCounty))]
+        public string? BillingCounty { get; set; }
+
+        [Required(ErrorMessage = nameof(BillingCountry))]
+        public string? BillingCountry { get; set; }
+
+        [MaxLength(20, ErrorMessage = nameof(BillingPostcode))]
+        public string? BillingPostcode { get; set; }
     }
 }

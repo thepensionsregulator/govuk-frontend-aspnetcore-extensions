73a614613cbc62271b62be5f48c4e277da172a08
Rick Mason <rick.mason@tpr.gov.uk>
2026-03-13 14:21:31 +0000

    Update to govuk-frontend-aspnetcore v3.5.1 and GOV.UK Frontend v5.14.0

--- ThePensionsRegulator.Frontend/wwwroot/ThePensionsRegulator.Frontend/js/tpr-search-results.js
+++ ThePensionsRegulator.Frontend/wwwroot/ThePensionsRegulator.Frontend/js/tpr-search-results.js
@@ -1,4 +1,4 @@
-﻿import { Accordion } from '/govuk-frontend.min.js?v=5.13.0';
+﻿import { Accordion } from '/govuk-frontend.min.js?v=5.14.0';
 
 let searchResults = [];
 let popularContentApiUrl = "";

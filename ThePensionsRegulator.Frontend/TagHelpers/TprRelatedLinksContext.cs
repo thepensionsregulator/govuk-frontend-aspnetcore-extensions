a7290d50ce44a4e1431fa1247f90182ce72413cd
Lambourne, Ethan <Ethan.Lambourne@tpr.gov.uk>
2026-04-01 17:31:20 +0100

    Revert "Automating aria-labelledby attribute; updating Umbraco and ASP.NET views to reflect new labelling and automation."
    This reverts commit 9c96bea9fc8009f10d82eb0044de5d662207cd50.

--- ThePensionsRegulator.Frontend/TagHelpers/TprRelatedLinksContext.cs
+++ ThePensionsRegulator.Frontend/TagHelpers/TprRelatedLinksContext.cs
@@ -1,7 +1,6 @@
 ﻿using GovUk.Frontend.AspNetCore;
 using Microsoft.AspNetCore.Html;
 using Microsoft.AspNetCore.Mvc.ViewFeatures;
-using System;
 using System.Collections.Generic;
 
 namespace ThePensionsRegulator.Frontend.TagHelpers
@@ -16,11 +15,6 @@ namespace ThePensionsRegulator.Frontend.TagHelpers
 
         public void AddLink(AttributeDictionary attributes, IHtmlContent htmlContent)
         {
-            if (!attributes.ContainsKey("aria-labelledby") && HeadingAttributes.ContainsKey("id"))
-            {
-                attributes.Add("aria-labelledby", HeadingAttributes["id"]);
-            }
-
             Links.Add((attributes, htmlContent));
         }
 
@@ -31,13 +25,6 @@ namespace ThePensionsRegulator.Frontend.TagHelpers
                 throw ExceptionHelper.OnlyOneElementIsPermittedIn(
                     TprRelatedLinksHeadingTagHelper.TagName,
                     TprRelatedLinksTagHelper.TagName);
-            }
-
-            if (!attributes.ContainsKey("id"))
-            {
-                var headingId = new KeyValuePair<string, string?>("id", Guid.NewGuid().ToString());
-                attributes.Add(headingId);
-                HeadingAttributes.Add(headingId);
             }
 
             _heading = (attributes, htmlContent);

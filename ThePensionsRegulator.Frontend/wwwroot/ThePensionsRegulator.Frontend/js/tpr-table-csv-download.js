465889df4ef196ffe3600c05ac6613ccfbdf64ee
Max-Emery <max.emery705@gmail.com>
2026-06-02 08:48:20 +0100

    #268154 Download table data button is duplicated in the html table component (v17) (#816)
    * Ensure duplicate button is not added to tables with a hardcoded button.
    
    * Fix vulnerability
    
    ---------
    
    Co-authored-by: Emery, Max <Max.Emery@tpr.gov.uk>

--- ThePensionsRegulator.Frontend/wwwroot/ThePensionsRegulator.Frontend/js/tpr-table-csv-download.js
+++ ThePensionsRegulator.Frontend/wwwroot/ThePensionsRegulator.Frontend/js/tpr-table-csv-download.js
@@ -115,14 +115,15 @@ export function initTableCsvDownload() {
 
     for (let i = 0; i < tables.length; i++) {
         const table = tables[i];
+        const nextElement = table.nextElementSibling;
 
         // Skip tables with merged cells — CSV cannot represent them reliably
         if (hasMergedCells(table)) continue;
 
         // Skip if a CSV download button has already been added (idempotency)
         if (
-            table.nextElementSibling &&
-            table.nextElementSibling.hasAttribute("data-tpr-table-csv-button")
+            nextElement &&
+            nextElement.hasAttribute("data-tpr-table-csv-button") || nextElement && nextElement.matches('form[class="tpr-table-download-form"]') && nextElement.querySelector("button")
         ) {
             continue;
         }

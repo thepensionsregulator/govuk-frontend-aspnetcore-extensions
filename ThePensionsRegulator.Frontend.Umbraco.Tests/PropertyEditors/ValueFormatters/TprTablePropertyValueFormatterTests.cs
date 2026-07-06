ab5798f8dbc5aa8caf899ff4673a4fb4c543e1df
Beesley <david.beesley@tpr.gov.uk>
2026-07-06 10:33:56 +0100

    Fix table formatter tests for new constructor dependencies

--- ThePensionsRegulator.Frontend.Umbraco.Tests/PropertyEditors/ValueFormatters/TprTablePropertyValueFormatterTests.cs
+++ ThePensionsRegulator.Frontend.Umbraco.Tests/PropertyEditors/ValueFormatters/TprTablePropertyValueFormatterTests.cs
@@ -1,4 +1,9 @@
-﻿using ThePensionsRegulator.Frontend.Umbraco.PropertyEditors.ValueFormatters;
+﻿using Microsoft.AspNetCore.Antiforgery;
+using Microsoft.AspNetCore.Http;
+using Microsoft.Extensions.Options;
+using Moq;
+using ThePensionsRegulator.Frontend;
+using ThePensionsRegulator.Frontend.Umbraco.PropertyEditors.ValueFormatters;
 using ThePensionsRegulator.Umbraco.Testing;
 using Umbraco.Cms.Core.Strings;
 
@@ -10,7 +15,7 @@ namespace ThePensionsRegulator.Frontend.Umbraco.Tests.PropertyEditors.ValueForma
         public void Applies_to_rich_text_properties_with_any_alias()
         {
             // Arrange
-            var formatter = new TprTablePropertyValueFormatter();
+            var formatter = CreateFormatter();
             var property = UmbracoPropertyFactory.CreateRichTextProperty(
                 Guid.NewGuid().ToString(),
                 "someContentType",
@@ -27,7 +32,7 @@ namespace ThePensionsRegulator.Frontend.Umbraco.Tests.PropertyEditors.ValueForma
         public void Does_not_apply_to_textbox_property()
         {
             // Arrange
-            var formatter = new TprTablePropertyValueFormatter();
+            var formatter = CreateFormatter();
             var property = UmbracoPropertyFactory.CreateTextboxProperty(
                 Guid.NewGuid().ToString(),
                 "someContentType",
@@ -46,7 +51,7 @@ namespace ThePensionsRegulator.Frontend.Umbraco.Tests.PropertyEditors.ValueForma
             // Arrange
             const string INPUT = "<table><tr><td>Example</td><td>Example</td></tr></table>";
             const string EXPECTED = $"<table class=\"{TprClassNames.Table}\"><tr><td>Example</td><td>Example</td></tr></table>";
-            var formatter = new TprTablePropertyValueFormatter();
+            var formatter = CreateFormatter();
 
             // Act
             var resultOfString = formatter.FormatValue(INPUT);
@@ -63,7 +68,7 @@ namespace ThePensionsRegulator.Frontend.Umbraco.Tests.PropertyEditors.ValueForma
             // Arrange
             const string INPUT = "<table class=\"existing-class\"><tr><td>Example</td><td>Example</td></tr></table>";
             const string EXPECTED = $"<table class=\"existing-class {TprClassNames.Table}\"><tr><td>Example</td><td>Example</td></tr></table>";
-            var formatter = new TprTablePropertyValueFormatter();
+            var formatter = CreateFormatter();
 
             // Act
             var resultOfHtmlEncodedString = formatter.FormatValue(new HtmlEncodedString(INPUT));
@@ -78,7 +83,7 @@ namespace ThePensionsRegulator.Frontend.Umbraco.Tests.PropertyEditors.ValueForma
             // Arrange
             const string INPUT = $"<table class=\"{TprClassNames.Table}\"><tr><td>Example</td><td>Example</td></tr></table>";
             const string EXPECTED = $"<table class=\"{TprClassNames.Table}\"><tr><td>Example</td><td>Example</td></tr></table>";
-            var formatter = new TprTablePropertyValueFormatter();
+            var formatter = CreateFormatter();
 
             // Act
             var resultOfHtmlEncodedString = formatter.FormatValue(new HtmlEncodedString(INPUT));
@@ -93,13 +98,22 @@ namespace ThePensionsRegulator.Frontend.Umbraco.Tests.PropertyEditors.ValueForma
             // Arrange
             const string INPUT = "<h2>Example</h2><p>Example</p><strong>Example</strong>";
             const string EXPECTED = INPUT;
-            var formatter = new TprTablePropertyValueFormatter();
+            var formatter = CreateFormatter();
 
             // Act
             var resultOfHtmlEncodedString = formatter.FormatValue(new HtmlEncodedString(INPUT));
 
             // Assert
             Assert.Equal(EXPECTED, ((HtmlEncodedString)resultOfHtmlEncodedString)?.ToHtmlString());
+        }
+
+        private static TprTablePropertyValueFormatter CreateFormatter()
+        {
+            var options = Options.Create(new TprFrontendOptions { EnableTableCsvDownload = false });
+            var antiforgery = Mock.Of<IAntiforgery>();
+            var httpContextAccessor = new HttpContextAccessor();
+
+            return new TprTablePropertyValueFormatter(options, antiforgery, httpContextAccessor);
         }
     }
 }

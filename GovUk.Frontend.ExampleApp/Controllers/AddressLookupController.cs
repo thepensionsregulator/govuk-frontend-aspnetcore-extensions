16dd53e95a899b9b149f39bb64454f2acd823245
Harry Ammon <harry.ammon@tpr.gov.uk>
2026-08-26 09:37:42 +0100

    Add example page for the address lookup

---
+++ GovUk.Frontend.ExampleApp/Controllers/AddressLookupController.cs
@@ -0,0 +1,12 @@
+﻿using Microsoft.AspNetCore.Mvc;
+
+namespace GovUk.Frontend.ExampleApp.Controllers
+{
+    public class AddressLookupController : BaseController
+    {
+        public IActionResult Index()
+        {
+            return View();
+        }
+    }
+}
